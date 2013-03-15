41eebcfb90076cf3465f93f69e560c69c6989098
Remi Bertin <remiber@free.fr>
2013-03-15 15:20:45 +0000

    Implemented first version of 108.pl Basic way. There is no optimization, ie it always go down the recursive tree down to pi = 2. Works for 108 (of course). need BigInts for 110. Then crash cause of bug

--- 108.pl
+++ 108.pl
@@ -3,22 +3,22 @@ use warnings;
 use Data::Dumper;
 use Prime;
 
-# Not used ?
-use Pythagoriciens;
-use Hashtools;
 use POSIX qw/floor ceil/;
+use List::Util qw( sum min max );
+use Math::BigInt;
 
 # The number of solutions S(n) of 1/x + 1/y = 1/n may be calculated :
 # If n = prod( pi^ki ), S(n) = (prod( 2*ki + 1) + 1)/2.
 # The idea is to find the least number n with S(n) >= M.
 # The equivalent problem PROB(m) solves :
-# minimize C= sum(Xi) such that  P >=  prod(Xi)
+# minimize C= sum(ai x Xi) such that  P >=  prod(Xi)
 # with :
-# Xi = (2ki+1)*ln(pi)
-# P = (2*M - 1)/prod( ln(pi))
+# Xi = (2ki+1)
+# P = (2*M - 1)
+# ai = log( pi )
  
 
-my($stop_at_first_ocuurence_of)= 1000;
+my($stop_at_first_ocuurence_of)= 4000000;
 
 my($max_for_odd_product)=( $stop_at_first_ocuurence_of )*2 -1;
 my($num_primes)=ceil( log($max_for_odd_product)/log(3) );
@@ -35,43 +35,55 @@ while( $#prime_tab+1 < $num_primes )
     push( @prime_prod_cumulated, $p * $prime_prod_cumulated[-1] );
     push( @kis, 0 );
 }
-    
-my($best_prod)= $prime_prod_cumulated[-1];
-$kis[ -1 ] = 0;
+my($best_prod)= Math::BigInt->new($prime_prod_cumulated[-1]);
+recursive_solve_PROB( $#kis-1, 0  , \@kis,  Math::BigInt->new(1), \$best_prod  );  
 
-recursive_solve_PROB( $#kis-1, 0  , \@kis,  1, \$best_prod  );  
+print $best_prod;
 
 sub recursive_solve_PROB
 {
-    my($j,$rkis, $minki, $current_prod, $rbestprod ) = @_;
+  my($idx, $base_ki, $rkis, $current_prod, $rbestprod ) = @_;
+  my($max_ki) = calc_max_ki ( $rbestprod, $current_prod, $idx ) + $base_ki;
+  
+  print Dumper \@_; 
+  print "MAX_ki : $max_ki\n";<STDIN>;
+  
+  if( $idx == 0 )
+  {
+    my($curent_ki_prod)= prod_kis( $rkis );
+    my( $min_Xi ) =  $max_for_odd_product/ ($curent_ki_prod/ (2*$base_ki+1) ) ;
+    my( $min_ki ) =  max( 0, ceil( ($min_Xi -1)/2 ) );
     
-    if( $j == 0 )
+    if( $min_ki > $max_ki )
     {
-	
-
+      return 0;
     }
     else
     {
-	
-	for(my($i)=0;$i<=$j;$i++)
-	{
-	    $$rkis[$i] = $minki;
-	}
-	
-	while( $current_prod < $$rbestprod )
-	{
-	    if( prod_kis( $rkis ) >= $max_for_odd_product )
-	    {
-		recursive_solve_PROB( $j-1, $minki, $current_prod, $rbestprod );
-	    }
-	    $minki++;
-	    $current_prod *= $prime_prod_cumulated[ $j ];
-	    for(my($i)=0;$i<=$j;$i++)
-	    {
-		$$rkis[$i] ++;
-	    }
-	}
+      $$rbestprod = $current_prod * ($prime_prod_cumulated[ $idx ] ** ($min_ki - $base_ki) );
+      return 1;
     }
+  }
+  else
+  {
+    $current_prod*= $prime_prod_cumulated[ $idx ] ** ($max_ki - $base_ki);
+    for(my($ki)=$max_ki;$ki>=$base_ki;$ki--)
+    {
+        for(my($j)=0;$j<=$idx;$j++)
+        {
+          $$rkis[$j] = $ki;
+        }
+        
+        recursive_solve_PROB( $idx - 1, $ki, $rkis, $current_prod, $rbestprod ); 
+        $current_prod/= $prime_prod_cumulated[ $idx ];
+    }
+  }
+}
+
+sub calc_max_ki
+{
+  my( $rbestprod, $current_prod, $idx ) =@_;
+  return floor( log($$rbestprod->bstr/$current_prod->bstr) / log( $prime_prod_cumulated[ $idx ] ) );
 }
 
 sub prime_for_kis
@@ -81,17 +93,15 @@ sub prime_for_kis
     my($step)=0;
     for(my($i)=$#$rkis;$i>=0;$i--)
     {
-	if( $$rkis[$i] == $step)
-	{
-	    next;
-	}
-	$product*=($$rprimes_cumulated[$i])**($$rkis[$i] - $step);
-	$step+=$$rkis[$i];
+      if( $$rkis[$i] == $step)
+      {
+          next;
+      }
+      $product*=($$rprimes_cumulated[$i])**($$rkis[$i] - $step);
+      $step+=$$rkis[$i];
     }
     return $product;
 }
-
-
 
 sub prod_kis
 {
@@ -99,10 +109,7 @@ sub prod_kis
     my($product)=1;
     for(my($i)=0;$i<=$#$rkis;$i++)
     {
-	$product*=(2*$$rkis[$i]+1); 
+	    $product*=(2*$$rkis[$i]+1); 
     }
     return $product;
 }
-
-   
-
