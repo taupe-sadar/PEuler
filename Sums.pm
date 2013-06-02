e1a0d026d380433eae18e36a24b58f527b20dd90
Remi Bertin <remiber@free.fr>
2013-06-02 10:51:55 +0000

    Indent

--- Sums.pm
+++ Sums.pm
@@ -8,48 +8,48 @@ sub int_sum
 
 sub int_sum_offset
 {
-    my($offset,$quantity)=@_;
-    if( $quantity <= 0 )
-    {
-	return 0;
-    }
-    elsif( $offset == 0 )
-    {
-	return int_sum( $quantity );
-    }
-    
-    return $offset*$quantity + int_sum( $quantity-1);
+  my($offset,$quantity)=@_;
+  if( $quantity <= 0 )
+  {
+    return 0;
+  }
+  elsif( $offset == 0 )
+  {
+    return int_sum( $quantity );
+  }
+  
+  return $offset*$quantity + int_sum( $quantity-1);
 }
 
 sub side_sum
 {
-    my($side,$n)=@_;
-    if($side==3)
-    {
-	return int_sum($n);
-    }
-    elsif($side==4)
-    {
-	return $n*$n;
-    }
-    elsif($side==5)
-    {
-	return $n*(3*$n-1)/2;
-    }
-    elsif($side==6)
-    {
-	return $n*(2*$n-1);
-    }
-    elsif($side==7)
-    {
-	return $n*(5*$n-3)/2;
-    }
-    elsif($side==8)
-    {
-	return $n*(3*$n-2);
-    }
-    
+  my($side,$n)=@_;
+  if($side==3)
+  {
+    return int_sum($n);
+  }
+  elsif($side==4)
+  {
+    return $n*$n;
+  }
+  elsif($side==5)
+  {
+    return $n*(3*$n-1)/2;
+  }
+  elsif($side==6)
+  {
+    return $n*(2*$n-1);
+  }
+  elsif($side==7)
+  {
+    return $n*(5*$n-3)/2;
+  }
+  elsif($side==8)
+  {
+    return $n*(3*$n-2);
+  }
 }
+
 sub int_square_sum
 {
 	my($last)=@_;
@@ -59,10 +59,10 @@ sub int_square_sum
 #return sum of int(n/d)
 sub sum_integer_div_by
 {
-    my($n,$d)=@_;
-    my($q)=int($n/$d);
-    my($r)=$n%$d;
-    my($s)=$q*($q-1)/2*$d + $q*($r+1);
-    return $s;
+  my($n,$d)=@_;
+  my($q)=int($n/$d);
+  my($r)=$n%$d;
+  my($s)=$q*($q-1)/2*$d + $q*($r+1);
+  return $s;
 }
 1;
