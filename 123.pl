1e75c5d6d74e07da4b479f92b12358f14c7a089e
Remi Bertin <remiber@free.fr>
2013-06-21 17:45:13 +0000

    Small bug. Only odd indexed primes counts

--- 123.pl
+++ 123.pl
@@ -7,12 +7,16 @@ my($max)=10**10;
 Prime::init_crible(10**6);
 my($prime_counter)=1;
 my($p)=Prime::next_prime();#Flushing 2 always ...
-my($reminder)=2;
-while($reminder <= $max )
+my($remainder)=2;
+while($remainder <= $max )
 {
+  # Skipping even values of prime_counter, leading to
+  # a remainder = 2
   $p=Prime::next_prime();
-  $prime_counter++;
-  $reminder=2*$prime_counter*$p;
+  $p=Prime::next_prime();
+  $prime_counter+=2;
+   
+  $remainder=2*$prime_counter*$p;
 }
 
 print $prime_counter;
