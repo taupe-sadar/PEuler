4d2dee4a81dd70f76b479b3b70f2f497eb294384
Remi Bertin <remiber@free.fr>
2013-06-26 13:38:24 +0000

    Remove part of algorithm not efficient. Add some other traces

--- 125.pl
+++ 125.pl
@@ -4,7 +4,7 @@ use Data::Dumper;
 use Sums;
 use POSIX qw/floor ceil/;
 
-my($max)=10**8;
+my($max)=10**3;
 my(@square_sums)=();
 
 my($sum)=0;
@@ -15,33 +15,7 @@ while( ($i**2 + ($i-1)**2) <= $max)
   $i++;
   $sum = Sums::int_square_sum( $i);
 }
-
-my($a_most_square_possible)=0;
-while( $square_sums[$a_most_square_possible +1] <= $max )
-{
-  $a_most_square_possible++;
-} 
-if(0)
-{
-open FILE, "> log.csv";
-for( my($a)=$#square_sums;$a>$a_most_square_possible;$a--)
-{
-  my($est_ind)= floor( (3*($square_sums[$a] - $max)  )**(1/3) );
-  print "$est_ind\n";
-  $est_ind-- while( $square_sums[$a] - $square_sums[$est_ind] <= $max );
-  $est_ind++ while( $square_sums[$a] - $square_sums[$est_ind] > $max );
-  
-  print FILE "$a;$est_ind;".($a - $est_ind ).";\n";
-  
-}
-for( my($a)=$a_most_square_possible;$a>1;$a--)
-{
-  print FILE "$a;0;".$a.";\n";
-}
-close FILE;
-print "".($#square_sums+1)."\n";
-}
-
+my($count)=0;
 my($sum_palindromes)=0;
 for( my($a)=$#square_sums;$a>1;$a--)
 {
@@ -52,11 +26,13 @@ for( my($a)=$#square_sums;$a>1;$a--)
     last if( $consecutive_squares > $max );
     if( is_palindromic( $consecutive_squares ) )
     {
+      print "$consecutive_squares\n";
       $sum_palindromes+= $consecutive_squares ;
+      $count ++;
     }
   }
 }
-print "--> $sum_palindromes\n";
+print "$count : $sum_palindromes\n";
 
 sub is_palindromic
 {
