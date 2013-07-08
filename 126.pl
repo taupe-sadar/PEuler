f075ee2665428c727082e1818367b94c1f35ac50
Remi Bertin <remiber@free.fr>
2013-07-08 23:02:55 +0000

    Limit exploring of a,b,c values, not exploring the highest trios. This works better: no more swapping, correct runtime for occurence = 100. Very long for 150. seems no end for n = 1000. gotta remove recording of cuboids no more necessary

--- 126.pl
+++ 126.pl
@@ -1,15 +1,16 @@
 use strict;
 use warnings;
 use Data::Dumper;
+use POSIX qw/floor ceil/;
 use List::Util qw( sum max min );
 use Hashtools;
 
-# Visiting all a >= b >= c >=1,
+# Visiting all 1 <= a <= b <= c ,
 # Looping over S = a + b + c.
 # After each S we have all cuboids with 
-# trio(a,b,c) = ab + ac + bc <= 2S + 1 
+# trio(a,b,c) = ab + ac + bc <= 2S - 3 
 
-my($num_layers)=10;
+my($num_layers)=150;
 
 my(@cuboids)=({},{},{});
 my(@layer_occurence_div2)=();
@@ -18,21 +19,22 @@ my($target_reached)=0;
 
 my($S) = 3;
 my($min_counting) = 3;
-my($milestone)= 10;
+my($milestone_layer)= 10;
 
 while( !$target_reached )
 {
-  visit_a_b_c_cuboids( $S );
-  if( $S >= $milestone )
+  visit_a_b_c_cuboids( $S , $milestone_layer  );
+  if( (2*$S - 3) > $milestone_layer )
   {
-    count_cuboids_no_greater( $min_counting, $S, 2*$S - 3   );
+    count_cuboids_no_greater( $min_counting, $S,  $milestone_layer  );
     #print Dumper \@cuboids;<STDIN>;
   
-    $target_reached = find_target_occurence( $num_layers, $min_counting, 2*$S  -3 );
+    $target_reached = find_target_occurence( $num_layers, $min_counting, $milestone_layer  );
     
-    $milestone*=2;
-    $min_counting = 2*$S - 2;
-     
+    $min_counting = $milestone_layer+1 ;
+    $milestone_layer*=10;
+    @cuboids = ({},{},{});
+    $S = 2;
   }
   $S++;
 }
@@ -46,18 +48,25 @@ print $target_reached*2;
 
 sub visit_a_b_c_cuboids
 {
-  my($sum)=@_;
-  my($mina)=$sum/3;
+  my($sum,$triomax)=@_;
+  my($maxa)=$sum/3;
+  if( $triomax < ($sum**2)/3 )
+  {
+    $maxa = floor( ( $sum - sqrt( $sum**2 - 3*$triomax ) )/3 );
+  }
+
   my(%cuboids_sum)=();
-  for(my($a)=$sum-2;$a>= $mina; $a--)
+  for(my($a)=1;$a<= $maxa; $a++)
   {
     my($sum_minus_a)= $sum-$a;
-    my($minb)=$sum_minus_a/2;
-    my($firstb)= min($a, $sum_minus_a - 1);
-    for(my($b)=$firstb;$b>= $minb; $b--)
+    my($maxb)=$sum_minus_a/2;
+    for(my($b)=$a;$b<= $maxb; $b++)
     {
       my($c)= $sum_minus_a-$b;
       my($trio)=$a*$b + $c*($a + $b);
+      last if $trio > $triomax;
+
+      
       if( !exists( $cuboids_sum{ $trio } ) )
       {
         $cuboids_sum{$trio}={ "occurence" => 1 ,"layer" => 0 ,"value"=> 0 }; 
