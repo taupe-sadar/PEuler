ba4b06e882674670a923ea9c2aac73beac0d5581
Remi Bertin <remiber@free.fr>
2013-08-06 23:12:27 +0000

    Adding stop condition on c and kc, Remove debug traces

--- 127.pl
+++ 127.pl
@@ -17,36 +17,47 @@ my( $max_for_c ) = 1000;
 
 Radical::init_set( $max_for_c );
 
+# There is a special case a = 1, b = 2**n, c = 3**m
+# There is only one solution b = 8 and c = 9 (easy to proove)
+
+my( $sum_c )= 9;
+
 while( 1 )
 {
   my($radical_c,$rdecomposition) = Radical::next_radical(0);
-  print $radical_c;<STDIN>;
-  last if $radical_c > $max_for_c;
+  
+  
+  
   my($max_for_kc)= floor( $max_for_c/ $radical_c );
+  last if( $max_for_kc <= 3 );
   my( $rkcs ) = Radical::list_composites( $max_for_kc,@$rdecomposition );
-  print Dumper $rkcs; <STDIN>;
+  
   for( my($i)= 0; $i <= $#$rkcs; $i++ )
   {
     # Case b = 1
     my( $kc ) = $$rkcs[$i];
+    next if( $kc <= 3); #because excluding special case.
     my( $a_with_b1 ) = $radical_c * $kc - 1;
     my( $rad_a);
-    if(  ($rad_a = is_abc_hit( $a_with_b1, 1 , $kc ))> 0 )
+    if(  ($rad_a = is_a_valid_for_abc_hit( $a_with_b1, 1 , $kc ))> 0 )
     {
       my($c)= $radical_c * $kc;
       print "$a_with_b1 , 1 , $c | $rad_a < $kc \n";
     }
+    
+    
+    
   }
   
 }
 
-sub is_abc_hit
+sub is_a_valid_for_abc_hit
 {
   my($a,$radb,$kc)=@_;
   my($limit_rad_a ) = ceil( $kc / $radb );
   my($rad_a)=1;
   my( $nb )= $a;
-  print "----- $nb ---- \n"; 
+   
   do
   {
     
