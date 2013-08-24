a6ce24492672cdfb115f24ccdf5572b985ede076
Remi Bertin <remiber@free.fr>
2013-08-24 21:47:42 +0000

    Fix 134 : i was counting one extra prime

--- 134.pl
+++ 134.pl
@@ -16,14 +16,15 @@ my($p1 ) = Prime::next_prime();# flushing 3;
 my($p2 ) = Prime::next_prime();
 
 my($sum)=Math::BigInt->new(0);
-
+( $p1,$p2 ) = ( $p2 , Prime::next_prime() );
 while( $p1 <= $max )
 {
-  ( $p1,$p2 ) = ( $p2 , Prime::next_prime() );
+  
   my( $decimal_exponent ) = length( $p1 );
   my( $inverse ) = Bezout::znz_inverse( 10**$decimal_exponent, $p2 );
   my( $first_digits ) = (-$p1* $inverse)% $p2;
   
   $sum+= "$first_digits$p1";
+  ( $p1,$p2 ) = ( $p2 , Prime::next_prime() );
 }
 print $sum;
