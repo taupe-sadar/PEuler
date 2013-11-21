4a1163d3903fab134b75fed87dd973c29a343d7a
Remi Bertin <remiber@free.fr>
2013-11-21 14:43:55 +0000

    Suare methode is wrong, some solutions are missing ... using more exhaustive approach

--- 141.pl
+++ 141.pl
@@ -2,35 +2,42 @@ use strict;
 use warnings;
 use Data::Dumper;
 use Gcd;
+use POSIX qw/floor ceil/;
+
 
 my($limit)= 10**12;
 my($a,$b,$r);
 
-for( $b = 1; $b<100; $b++ )
+for( $b = 1; $b<10**6; $b++ )
 {
-  my($b2)= $b**2;
-  for( $a = $b2 + 1; $a < 10000; $a++ )
+  my($max_a) = ceil( ($limit/ $b)**(1/3) );
+  
+  for( $a = $b + 1; $a < $max_a; $a++ )
   {
+    if( Gcd::pgcd( $b, $a ) !=1 )
+    {
+      next;
+    }
+    
     $r =1 ;
     my($a3)=$a**3;
-    my($nn2);
-    while(1)
+    my($n2);
+    do
     {
-      my($r2)=$r**2;
-      my($n2) = $a3 * $r2 + $b2;
       
-      $nn2 = $n2*$b2*$r2;
-      last if( $nn2 > $limit);
-      if( !(sqrt($n2) =~m/\./ ) )
+      $n2 = $r*$b*($a3 * $r + $b);
+      my($n)= sqrt($n2);
+      if( !($n =~m/\./ ) )
       {
-        my($n)= $b*$r*sqrt( $n2);
-        my($r0)= $r2*$b2*$b2;
-        my($q)= ( $a * $r0 ) / $b2;
-        my($d) = ( $q * $a ) / $b2;
+        my($r0)= $r*$b*$b;
+        my($q)= ( $a * $r0 ) / $b;
+        my($d) = ( $q * $a ) / $b;
         print "b : $b, a : $a, r : $r -> $n^2 = $r0 + $q * $d \n";
         
       }
       $r++;    
     }
+    while( $n2 < $limit);
+    
   }
 }
