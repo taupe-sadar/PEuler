eb6e38a91e7f7e0f456ef8f1c7c0c945edb954d2
Remi Bertin <remiber@free.fr>
2014-05-21 16:06:49 +0000

    Starting new method

--- 142.pl
+++ 142.pl
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 use Data::Dumper;
 use Prime;
+use Bezout;
 use Permutations;
 use Math::BigInt;
 use Gcd;
@@ -12,11 +13,61 @@ my( @primes_1_mod_8) =();
 
 my( %square_components ) = ();
 
-for( my($i)=97;$i<1000;$i+=2 )
+for( my($i) = 3; $i< 1000; $i+=2 )
+{
+  my($i4) = Math::BigInt->new($i)**4;
+  for( my($j) = 1; $j< $i; $j+=2 )
+  {
+    next if( Gcd::pgcd( $i, $j ) > 1 );
+    my($j4) = Math::BigInt->new($j)**4;
+    my($sum4) = ($i4+$j4)/2;
+    my(%dec) = decompose_with_primes_1_mod_8( $sum4 );
+    
+    my($p)=1;
+    foreach my $k (keys(%dec))
+    {
+      $p *= $k if( $dec{$k} %2 == 1 );
+    }
+    if( $p == 1 )
+    {
+      print "interesting : p = 1\n";
+      <STDIN>;
+    }
+    else
+    {
+      my($x0)= $i%$p;
+      my($y0)= $j%$p;      
+      
+      my( $z0 ) = $x0* Bezout::znz_inverse( $y0, $p );
+      
+      my(@couples)=();
+      #Building valid couples
+      my( @xsols ) = ( $z0, $z0**3, -$z0, -($z0**3) );
+      for( my($y) = 1; $y < $p; $y++ )
+      {
+        for( my($s)= 0;$s<=$#xsols; $s++ )
+        {
+          my( $x ) = ($y*$xsols[$s])%$p;
+          push( @couples , [ $x, $y ] ) if $x <= $y;
+        }
+      }
+      # print $#couples; <STDIN>;
+      # print Dumper \@couples; <STDIN>;
+      
+      my( $virtual_bound ) = ($p*4)**4;
+      
+    }
+  }
+}
+
+
+exit(0);
+
+for( my($i)=1;$i<1000;$i+=2 )
 {
   my($i4) = Math::BigInt->new($i)**4;
   
-  for( my($j)=97;$j<$i;$j+=2 )
+  for( my($j)=1;$j<$i;$j+=2 )
   {
     next if( Gcd::pgcd( $i, $j ) > 1 );
     
@@ -32,7 +83,7 @@ for( my($i)=97;$i<1000;$i+=2 )
       $squ_component *= $k if( $dec{$k} %2 == 1 );
     }
 
-    print( "$i - $j -> $squ_component\n" );
+    # print( "$i - $j -> $squ_component\n" );
     # print "  ".(join(" ",keys(%dec)))." => ".(join(" ",values(%dec)))."\n";<STDIN>;
     # print "  ".(join(" ",keys(%dec2)))." => ".(join(" ",values(%dec2)))."\n";<STDIN>;
     
@@ -46,21 +97,20 @@ for( my($i)=97;$i<1000;$i+=2 )
       print "$squ_component\n";
       print Dumper $square_components{ $squ_component };
       
+      
       my($t,$w,$u,$v) = ( @{$square_components{ $squ_component }[0]} , @{$square_components{ $squ_component }[1]} );
       my($p,$q,$r,$s) = ( $t*$u , $v*$w , $t*$v, $w*$u );
       my( $b ,$c , $d, $e ,$f ) = ( ($p**2 + $q**2)/2 , ($p**2 - $q**2)/2, $p*$q, ($r**2 + $s**2)/2 , ($s**2 - $r**2)/2 );
       my( $a ) =  sqrt( Math::BigInt->new($b)**2 + Math::BigInt->new($f)**2) ;
       
       my($x,$y,$z)=( ($a**2 + $b**2 - $c**2)/2,  ($a**2 + $c**2 - $b**2)/2, ($b**2 + $c**2 - $a**2)/2 );
+      my($sum ) = $x+$y+$z;
       
       print "$p $q $r $s\n";
       print "$a $b $c $d $e $f\n";
-      print "$x $y $z ".($x+$y+$z)."\n";
+      print "$x $y $z -> $sum\n";
       
-      
-      
-      exit( 0 );
-     <STDIN>;      
+          
     }    
   }
 }
