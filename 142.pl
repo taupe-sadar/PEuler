f52cae24fa2fcc317108a5731610c7cce6505871
Remi Bertin <remiber@free.fr>
2016-07-17 21:26:17 +0200

    I have made a mistake : the square are not build from primitive pythagorian primes ... The best effort works much better than my gas factory ...

--- 142.pl
+++ 142.pl
@@ -6,6 +6,43 @@ use Bezout;
 use Permutations;
 use Math::BigInt;
 use Gcd;
+use POSIX qw/floor ceil/;
+use List::Util qw( max min );
+
+if(0)
+{
+
+for( my($bound) = 1000; 1; $bound*=10 )
+{
+  for(my($f)=1;$f<$bound;$f+=2)
+  {
+  for(my($b)=2;$b<$bound;$b+=2)
+  {
+    my($d2) = square($b) + square($f);
+    next unless( ts( $d2) );
+  
+  for(my($e)=2;$e<$bound;$e+=2)
+  {
+    next unless ( ts( square($e) + square($f)) );
+    next unless ( ts( square($e) + $d2) );
+  
+    print "Found all squares : b = $b, e = $e, f = $f\n";
+  }
+  }
+  }
+} 
+
+
+exit(0);
+}
+
+sub ts
+{
+  my($x)=@_;
+  return (square(int(sqrt($x))) == $x);
+}
+
+
 
 my( @primes_1_mod_8) =();
 
@@ -18,18 +55,31 @@ my( $best_final_sum ) = 0;
 my($stop) = 0;
 my($first_pass ) = 1;
 
-find_this_quad_sum( 3, 1 );
-
+find_this_quad_sum( 3, 1, 1, 1 );
 $first_pass = 0;
 
-for( my($i) = 5; !$stop; $i+=2 )
+for( my($max)=3; !$stop; $max+=2)
 {
-  for( my($j) = 1; $j< $i; $j+=2 )
+  for( my($i) = 3; $i<=$max; $i+=2 )
   {
-    next if( Gcd::pgcd( $i, $j ) > 1 );
-    
-    # print "Start couple $i $j\n";
-    find_this_quad_sum( $i,$j );
+    for( my($j) = 1; $j< $i; $j+=2 )
+    {
+      next if( Gcd::pgcd( $i, $j ) > 1 );
+      for( my($lambda) = 1; $lambda<= $max; $lambda+=2 )
+      {
+        my($start_mu)=1;
+        $start_mu = $max unless( $lambda == $max || $i == $max );
+        
+        for( my($mu) = $start_mu; $mu<= $max; $mu+=2 )
+        {
+          next if( Gcd::pgcd( $lambda, $mu ) > 1 );
+          
+          print "Start couple $i $j ($lambda $mu)\n";
+          find_this_quad_sum( $i,$j, $lambda, $mu );
+          
+        }
+      }
+    }
   }
 }
 
@@ -58,7 +108,7 @@ for( my($i)=1;$i<1000;$i+=2 )
       $squ_component *= $k if( $dec{$k} %2 == 1 );
     }
 
-    # print( "$i - $j -> $squ_component\n" );
+    print( "$i - $j -> $squ_component\n" );
     # print "  ".(join(" ",keys(%dec)))." => ".(join(" ",values(%dec)))."\n";<STDIN>;
     # print "  ".(join(" ",keys(%dec2)))." => ".(join(" ",values(%dec2)))."\n";<STDIN>;
     
@@ -92,10 +142,10 @@ for( my($i)=1;$i<1000;$i+=2 )
 
 sub find_this_quad_sum
 {
-  my($i,$j)=@_;
-  my($i4) = Math::BigInt->new($i)**4;
-  my($j4) = Math::BigInt->new($j)**4;
-  my($sum4) = ($i4+$j4)/2;
+  my($i,$j, $lambda, $mu)=@_;
+  my($i4) = bisquare(Math::BigInt->new($i));
+  my($j4) = bisquare(Math::BigInt->new($j));
+  my($sum4) = (square($lambda)*$i4+square($mu)*$j4)/2;
   my(%dec) = decompose_with_primes_1_mod_8( $sum4 );
   
   my($p)=1;
@@ -113,20 +163,21 @@ sub find_this_quad_sum
       $y0 = $i%$p;
     }
     
-    my( $z0 ) = $x0* Bezout::znz_inverse( $y0, $p );
+    my( $z0 ) = Math::BigInt->new( $x0* Bezout::znz_inverse( $y0, $p ));
     
-    my($trivial_bound)= sum_quadring( $x0, $y0); 
+    my($trivial_bound)= sum_quadring( $x0, $y0, $lambda, $mu); 
     
     my($last_passes_val)=0;
     if( !$first_pass )
     {
       $last_passes_val = int( sqrt(sqrt($current_a_square/$trivial_bound))) + 1;
+      print "Last val : $last_passes_val\n";
     }
-    
     
     my(@couples)=();
     #Building valid couples
-    my( @xsols ) = ( $z0, $z0**3, -$z0, -($z0**3) );
+   
+    my( @xsols ) = ( $z0, cube($z0), -$z0, -cube($z0) );
     for( my($y) = 1; $y < $p; $y++ )
     {
       last if( !$first_pass && $y > $last_passes_val );
@@ -139,24 +190,26 @@ sub find_this_quad_sum
         push( @couples , [ $y, $x ] ) if $x >= $y;
       }
     }
-    
-    # print Dumper \@couples;    <STDIN>;
-    
-    if( $j == 1 && $#current_quad_quad_solution >= 0 && $trivial_bound**2 > $current_a_square)
+     
+    my($trivial_bound_forall)= sum_quadring( $x0, $y0, 1, 1);
+    if( $j == 1 && $#current_quad_quad_solution >= 0 && square($trivial_bound_forall) > ($current_a_square))
     {
+      print " Stopping ".(square($trivial_bound))." > $current_a_square\n";
+      
       $stop = 1;
-      last;
+      return;
     }
     
-    for( my($bound) = 0; ($#current_quad_quad_solution < 0) || (sum_quadring( $bound*$p + 1, 1) * $trivial_bound <= $current_a_square); $bound++ )
+    for( my($bound) = 0; ($#current_quad_quad_solution < 0) || (sum_quadring( $bound*$p + 1, 1,$lambda,$mu) * $trivial_bound <= $current_a_square); $bound++ )
     {
+     
       # print "BOUND $bound\n";
       # print "Couples : ".($#couples+1)."\n";
       
       my(@sols)=();
       for( my( $h ) = 0 ; $h <= $bound; $h++)
       {
-        my(@idxs) = find_sol_square( \@couples, $bound, $h, $p );
+        my(@idxs) = find_sol_square( \@couples, $bound, $h, $p, $lambda, $mu );
         for(my( $ix ) = 0 ; $ix <= $#idxs; $ix++ )
         {
           push( @sols, [$couples[$idxs[$ix]][0] + $bound*$p, $couples[$idxs[$ix]][1] + $h*$p]);
@@ -164,24 +217,24 @@ sub find_this_quad_sum
       }
       for( my( $h ) = 0 ; $h < $bound; $h++)
       {
-        my(@idxs) = find_sol_square( \@couples, $h, $bound, $p );
+        my(@idxs) = find_sol_square( \@couples, $h, $bound, $p, $lambda, $mu );
         for(my( $ix ) = 0 ; $ix <= $#idxs; $ix++ )
         {
           push( @sols, [$couples[$idxs[$ix]][0] + $h*$p, $couples[$idxs[$ix]][1] + $bound*$p]);
         }
       }
+       
       
       for(my( $i ) = 0 ; $i <= $#sols; $i++ )
       {
         my($rsol) = $sols[$i];
         next if( $$rsol[0] == $x0 && $$rsol[1] == $y0 );
         
-        my($new_square) = sum_quadring( $$rsol[0], $$rsol[1]);  
+        my($new_square) = sum_quadring( $$rsol[0], $$rsol[1],$lambda,$mu);  
         
-        # print "$x0,$y0, $$rsol[0], $$rsol[1]\n";
-        
-        my($newsum ) = final_sum($x0,$y0,@$rsol);
-        if( $#current_quad_quad_solution < 0 || $newsum < $best_final_sum)
+        my($x,$y,$z) = final_numbers($x0,$y0,@$rsol, $lambda, $mu);
+        my($newsum)= $x + $y +$z;
+        if( $#current_quad_quad_solution < 0 || ($x > 0 && $y>0 && $z > 0 && $newsum < $best_final_sum))
         {
           @current_quad_quad_solution = @$rsol;
           $current_a_square = $trivial_bound * $new_square;
@@ -196,14 +249,14 @@ sub find_this_quad_sum
 
 sub final_sum
 {
-  my($t,$w,$u,$v)=@_;
-  my($x,$y,$z) = final_numbers( $t,$w,$u,$v );
+  my($t,$w,$u,$v, $lambda, $mu)=@_;
+  my($x,$y,$z) = final_numbers( $t,$w,$u,$v, $lambda, $mu );
   return ($x+$y+$z);
 }
 
 sub final_numbers
 {
-  my($t,$w,$u,$v) = @_;
+  my($t,$w,$u,$v, $lambda, $mu) = @_;
   if( $t < $w )
   {
     ($t,$w) = ($w,$t);
@@ -218,14 +271,15 @@ sub final_numbers
   }
   
   my($p,$q,$r,$s) = ( Math::BigInt->new($t*$u) , Math::BigInt->new($v*$w) , Math::BigInt->new($t*$v), Math::BigInt->new($w*$u) );
-  my( $b ,$c , $d, $e ,$f ) = ( ($p**2 - $q**2)/2 , ($p**2 + $q**2)/2, ($r**2 + $s**2)/2 , ($r**2 - $s**2)/2, $p*$q );
-  my( $a ) =  sqrt(($p**4 + $q**4 + $r**4 + $s**4)/4) ;
+  my( $b ,$c , $d, $e ,$f ) = ( $lambda*(square($p) - square($q))/2 , $lambda*(square($p) + square($q))/2, $mu*(square($r) + square($s))/2 , $mu*(square($r) - square($s))/2, $lambda*$p*$q );
+  my( $a ) =  sqrt((square($lambda)*(bisquare($p)+bisquare($q))+square($mu)*(bisquare($r)+bisquare($s)))/4) ;
   
   my($x,$y,$z)=( ($a**2 + $c**2 - $b**2)/2,  ($a**2 + $b**2 - $c**2)/2, ($b**2 + $c**2 - $a**2)/2 );
    
   my($sum ) = $x+$y+$z;
   
   print "-------------\n";
+  print "($lambda $mu)\n";
   print "$t $w $u $v\n";
   print "$p $q $r $s\n";
   print "$a $b $c $d $e $f\n";
@@ -240,13 +294,13 @@ sub final_numbers
 
 sub sum_quadring
 {
-  my( $x, $y) = @_;
-  return (Math::BigInt->new($x)**4 + Math::BigInt->new($y)**4)/2;
+  my( $x, $y, $lambda, $mu) = @_;
+  return (square($lambda)*bisquare(Math::BigInt->new($x)) + square($mu)*bisquare(Math::BigInt->new($y)))/2;
 }
 
 sub find_sol_square
 {
-  my($rcouples, $m, $n,$p ) = @_;
+  my($rcouples, $m, $n,$p, $lambda, $mu ) = @_;
   my(@ret)=();
   for( my($a)=0;$a<=$#$rcouples;$a++)
   {
@@ -256,15 +310,33 @@ sub find_sol_square
     next if(($x%2 == 0) ||( $y%2 == 0));
     next if( Gcd::pgcd( $x, $y ) > 1 );
     
-    my($square)= sum_quadring($x,$y)/$p;
+    my($square)= sum_quadring($x,$y, $lambda, $mu)/$p;
     my($root) = int(sqrt($square));
     
-    push(@ret,$a) if( $root**2 == $square) ;
+    push(@ret,$a) if( $root*$root == $square) ;
   }
   return @ret;
 }
 
-
+sub square
+{
+  my($x)=@_;
+  if( !defined($x))
+  {
+    print "undef\n";<STDIN>;
+  }
+  return $x*$x;
+}
+sub cube
+{
+  my($x)=@_;
+  return square($x)*$x;
+}
+sub bisquare
+{
+  my($x)=@_;
+  return square(square($x));
+}
 
 sub decompose_with_primes_1_mod_8
 {
