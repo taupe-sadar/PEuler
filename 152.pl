2ce9e3dca9447a9f19d0a382fd586d36540e6d33
Remi Bertin <remiber@free.fr>
2017-01-05 00:53:09 +0100

    Rewriting everything removing too coplex code. But now it is buggy ...

--- 152.pl
+++ 152.pl
@@ -6,12 +6,14 @@ use Prime;
 use Bezout;
 use Fraction;
 
-my($max_integer)=65;
+my($max_integer)=45;
 
 
-my(%cool_additions)=();
-
-my(%known_restrictions)=();
+my(@cool_additions)=(
+{
+  "numbers" => {},
+  "value" => (Fraction->new(0,1))
+});
 
 my($rworking_divisors)=build_working_divisors();
 
@@ -101,36 +103,12 @@ sub analyze_with_p_factors
   my($pexp2)=$pexp*$pexp;
   my($p2)=$p*$p;
   
-  my($highest_factor)= floor( $max_integer/$pexp );
-  my($nb)=$pexp;
+  my(%hypo_vals)=browse_hypothesis(\@cool_additions,$pexp,$p);
   
-  my(%previous_hypothesis)=();
-  my(%not_used_hypos)=();
-  
-  for( my($k)=1;$k<=$highest_factor;$k++)
-  {
-    if( exists( $known_restrictions{ $nb } ))
-    {
-      if( $known_restrictions{ $nb }{"type"} ne "NONE" )
-      {
-        retrieve_concern_hypothesis( $nb, \%previous_hypothesis);
-      }
-    }
-    $nb+=$pexp;
-  }
-  
-  my(%hypo_vals)=browse_hypothesis(\%previous_hypothesis,$pexp,$p);
-  
+ 
   
   my(@inverses)=map({Bezout::znz_inverse($_*$_,$p2)} @$rfactors);
   my(@sols)=packsack_solutions( \@inverses, $p2, \%hypo_vals );
-  
-  $nb=$pexp;
-  for( my($k)=1;$k<=$highest_factor;$k++)
-  {
-    $known_restrictions{ $nb } = {"type" => "NONE"};
-    $nb+=$pexp;
-  }
   
   if( $#sols >= 0 )
   {
@@ -147,8 +125,7 @@ sub analyze_with_p_factors
       <STDIN>;
     }
     
-    my($cool_id)="Hypo-$pexp";
-    $cool_additions{$cool_id} = [];
+    @cool_additions = ();
     for( my($i)=0;$i<=$#sols;$i++)
     {
       my(%sol_numbers)=();
@@ -159,15 +136,13 @@ sub analyze_with_p_factors
         $sol_numbers{$$rfactors[$sols[$i]{"idxs"}[$j]]*$pexp}=1;
       }
       
-      
-      
       my($frac)= build_hypo_frac( $pexp/$p, \%sol_numbers);
       
       my($hypo_val)= $sols[$i]{"hypo_val"};
       my($previous_hypos_vals)=$hypo_vals{$hypo_val};
       for(my($i)=0;$i<=$#$previous_hypos_vals;$i++)
       {
-        my($new_frac)=retrieve_hypo_frac($$previous_hypos_vals[$i]{"hypos"});
+        my($new_frac)=$$previous_hypos_vals[$i]{"value"};
         # print "*** $x ***\n";
         # print "***  ".($x+$frac)." ***\n";
         my(%new_sol_numbers)=();
@@ -186,130 +161,44 @@ sub analyze_with_p_factors
         # }
         
         
-        push( @{$cool_additions{$cool_id}}, 
+        push( @cool_additions, 
         {
           "numbers" => \%new_sol_numbers,
           "value" => ($frac + $new_frac)
         });
       }
     }
-    
-    rebuild_restrictions();
   }
   
-  foreach my $cool_id2 (keys(%cool_additions))
-  {
-    my($class)=$cool_additions{$cool_id2};
-    my(@new_hypos)=();
-    my(@concerned)=();
-    for( my($i)=0;$i<=$#$class;$i++ )
-    {
-      if( !exists($previous_hypothesis{$cool_id2}) || !exists($previous_hypothesis{$cool_id2}{$i})) 
-      {
-        push(@new_hypos,$$class[$i]);
-      }
-    }
-    $cool_additions{$cool_id2} = \@new_hypos;
-  }
-  
-  if( $pexp <= 9 )
+  if( $pexp <= 27 )
   {
     print "--- $pexp ---\n";
-    foreach my $cool_id (keys(%cool_additions))
+    for( my($i)=0;$i<= $#cool_additions;$i++)
     {
-      my($id_additions)=$cool_additions{$cool_id};
-      for(my($i)=0;$i<=$#$id_additions;$i++)
-      {
-        print sprintf("%-12s",$cool_id."-$i");
-        print sprintf("%-16s","".$$id_additions[$i]{"value"});
-        my($id_add)=$$id_additions[$i]{"numbers"};
+        # print sprintf("%-12s",$cool_id."-$i");
+        print sprintf("%-10s","$pexp-$i");
+        print sprintf("%-16s","".$cool_additions[$i]{"value"});
+        my($id_add)=$cool_additions[$i]{"numbers"};
         foreach my $nb (sort({$a<=>$b}keys(%$id_add)))
         {
           print "$nb ";
         }
         print "\n";
-      }
     }
     <STDIN>;
   }
 }
 
-sub rebuild_restrictions
-{
-  foreach my $k (keys(%known_restrictions))
-  {
-    $known_restrictions{$k}{"type"}="NONE";
-  }
-  
-  foreach my $cool_id (keys(%cool_additions))
-  {
-    my($id_additions)=$cool_additions{$cool_id};
-    my(@restrictions)=();
-    for(my($i)=0;$i<=$#$id_additions;$i++)
-    {
-      my($id_add)=$$id_additions[$i]{"numbers"};
-      
-      foreach my $nb (keys(%$id_add))
-      {
-        my($super_cool_id)=$cool_id."_$i";
-        if( !exists($known_restrictions{$nb}) || $known_restrictions{$nb}{"type"} eq "NONE" )
-        {
-          $known_restrictions{$nb}{"type"} = "available";
-          $known_restrictions{$nb}{"hypos"} = [];
-        }
-        push( @{$known_restrictions{$nb}{"hypos"}}, $super_cool_id );
-      }
-    }
-  }
-}
-
-sub retrieve_hypo_frac
-{
-  my($rhypos)=@_;
-  my($frac)=Fraction->new(0,1);
-    
-  for(my($j)=0;$j<=$#$rhypos;$j++)
-  {
-    my($cool_addition)=getIdxHypo($$rhypos[$j]);
-    $frac = $frac + $$cool_addition{"value"};
-  }
-  return $frac;
-}
-
-sub retrieve_concern_hypothesis
-{
-  my($nb,$rhash,$rnot_used)=@_;
-  
-  my($rarray_cool_ids)=$known_restrictions{ $nb }{"hypos"};
-  for(my($j)=0;$j<=$#$rarray_cool_ids;$j++)
-  {
-    my($main,$idx)=split("_",$$rarray_cool_ids[$j]);
-    if( !exists($$rhash{$main}))
-    {
-      $$rhash{$main}={};
-    }
-    $$rhash{$main}{$idx}=$cool_additions{$main}[$idx];
-  }
-}
-
 sub browse_hypothesis
 {
-  my($rfetched_hypos,$pexp,$p)=@_;
-  
-  my(@combined)=loop_hypos($rfetched_hypos);
-  
-  if( $pexp == 3 )
-  {
-    print $#combined + 1; <STDIN>;
-  }
+  my($rallhypos,$pexp,$p)=@_;
   
   my(%values_hypos)=();
   my($p2)=$p*$p;
   my($pexp2)=$pexp*$pexp;
-  for(my($i)=0;$i<=$#combined;$i++)
+  for(my($i)=0;$i<=$#$rallhypos;$i++)
   {
-    my($rhypos)=$combined[$i]{"hypos"};
-    my($frac)=retrieve_hypo_frac($rhypos);
+    my($frac)=$$rallhypos[$i]{"value"};
     
     my($denom)=$frac->{"denominator"};
     my($boost)=1;
@@ -319,118 +208,17 @@ sub browse_hypothesis
       $denom *= $p;
     }
     
-    my($val) = $frac->{"numerator"}*$boost* Bezout::znz_inverse($denom/($pexp*$pexp),$p2);
+    my($val) = $frac->{"numerator"}*$boost* Bezout::znz_inverse($denom/$pexp2,$p2);
     $val = $val%$p2;
     
     if( !exists($values_hypos{$val}) )
     {
       $values_hypos{$val} = [];
     }
-    push( @{$values_hypos{$val}}, $combined[$i] );
+    push( @{$values_hypos{$val}}, $$rallhypos[$i] );
   }
   return %values_hypos;
 }
-
-
-sub loop_hypos
-{
-  my($rhypos)=@_;
-  #TODO la ou ca compile pas
-  my(@keys)=(keys(%$rhypos));
-  if( $#keys < 0)
-  {
-    return ({"numbers" => {},"hypos"=> []});
-  }
-  else
-  {
-    my($myhypo)=$keys[0];
-    my($rhyps)=$$rhypos{$myhypo};
-    
-    my(%others)=();
-    for(my($i)=1;$i<=$#keys;$i++)
-    {
-      $others{$keys[$i]} = $$rhypos{$keys[$i]};
-    }    
-    
-    my(@sols_others)=loop_hypos(\%others);
-    
-    my(@new_sols)=();
-    foreach my $hypothesis (keys(%$rhyps))
-    {
-      my($hyp_numbers)=$$rhyps{$hypothesis}{"numbers"};
-      for( my($a)=0;$a<=$#sols_others;$a++)
-      {
-        my($rused)=$sols_others[$a]{"numbers"};
-        my($valid)=1;
-        my(%new_numbers)=();
-        foreach my $n (keys( %$hyp_numbers ))
-        {
-          if( exists($$rused{$n}))
-          {
-            $valid = 0;
-            last;
-          }
-          $new_numbers{$n}=1;
-        }
-        if( $valid )
-        {
-          my(@new_hypos)=($myhypo.'_'.$hypothesis, @{$sols_others[$a]{"hypos"}});
-          foreach my $n (keys( %$rused ))
-          {
-            $new_numbers{$n}=1;
-          }
-          my($nsol)={"numbers" => \%new_numbers,"hypos"=> \@new_hypos};
-          if(!does_hypo_already_exists( $nsol , \@sols_others ))
-          {
-            push(@new_sols,$nsol);
-          }
-          else
-          {
-            print "Seriously ???\n"; 
-            print Dumper $nsol;
-            <STDIN>;
-          }
-          
-          
-        }
-      }
-    }
-    return (@sols_others,@new_sols);
-  }
-}
-
-sub getIdxHypo
-{
-  my($key_idx_hypo)=@_;
-  my($hypop,$num)=split("_",$key_idx_hypo);
-  return $cool_additions{$hypop}[$num];
-}
-
-sub does_hypo_already_exists
-{
-  my($rsol,$rall_sols)=@_;
-  my($rcandidate_numbers)=$$rsol{"numbers"};
-  my(@keys_cand)=(keys(%$rcandidate_numbers));
-  for( my($a)=0;$a<=$#$rall_sols; $a++ )
-  {
-    my($rthis_numbers)=$$rall_sols[$a]{"numbers"};
-    my(@key_this)=(keys(%$rthis_numbers));
-    next if( $#key_this != $#keys_cand );
-    
-    my($is_equal)=1;
-    for( my($b)=0; $b<= $#keys_cand; $b++ )
-    {
-      if( !exists($$rthis_numbers{$keys_cand[$b]} ))
-      {
-        $is_equal = 0;
-        last;
-      }
-    }
-    return 1 if($is_equal);
-  }
-  return 0;
-}
-
 
 sub build_hypo_frac
 {
@@ -441,9 +229,7 @@ sub build_hypo_frac
     my($factor)=$k;
     $f = $f + Fraction->new(1,$factor*$factor);
   }
-  
   return $f;
-  
 }
 
 sub packsack_solutions
@@ -460,17 +246,9 @@ sub packsack_solutions
     {
       my($x)=($allsums[$j]+$$rarray[$i])%$modulo;
       my($opposite_in_hypo)=(-$x)%$modulo;
-      
-      
-      
+
       if( exists($$rhypos{$opposite_in_hypo}) )
       {
-        # if( $modulo == 25 )
-        # {
-          # print "--- $x ($$rarray[$i])\n";
-        # }
-        
-        
         push(@sols,{"idxs"=>dec_2($allsums_size+$j),"hypo_val"=>$opposite_in_hypo});
       }
       
@@ -478,65 +256,9 @@ sub packsack_solutions
     }
   }
   
-  
-  # my(@reduced)=reduce_sols(@sols);
-  # for( my($i)=0;$i<= $#reduced; $i++ )
-  # {
-    # print "".join(" ",@{$reduced[$i]{"idxs"}})." - ".$reduced[$i]{"hypo_val"}."\n";
-  # }
-  
-  
   return @sols;
 }
 
-# sub reduce_sols
-# {
-  # my(@sols)=@_;
-  # my(@hashes)=();
-  # for(my($i)=0;$i<=$#sols;$i++)
-  # {
-    # my(%h)=();
-    # for(my($j)=0;$j<=$#{$sols[$i]{"idxs"}};$j++)
-    # {
-      # $h{$sols[$i]{"idxs"}[$j]}=1;
-    # }
-    # push(@hashes,\%h);
-  # }
-  
-  # my(@smallers)=();
-  # for(my($i)=0;$i<=$#hashes;$i++)
-  # {
-    # my(@keys_i)=(keys(%{$hashes[$i]}));
-    # my($is_redondant)=0;
-    # for(my($j)=0;$j<=$#hashes;$j++)
-    # {
-      # next if( $i == $j );
-      # next if( $sols[$j]{"hypo_val"} != 0 );
-      # my(@keys_j)=(keys(%{$hashes[$j]}));
-      # next if( $#keys_i < $#keys_j );
-      # my($j_is_included_in_i)=1;
-      # foreach my $k (@keys_j)
-      # {
-        # if( !exists($hashes[$i]{$k}) )
-        # {
-          # $j_is_included_in_i = 0;
-          # last;
-        # }
-      # }
-      # if( $j_is_included_in_i )
-      # {
-        # $is_redondant = 1;
-        # last;
-      # }
-    # }
-    # if( !$is_redondant )
-    # {
-      # push(@smallers,$sols[$i]);
-    # }
-  # }
-  # return  @smallers;
-  
-# }
 
 sub dec_2
 {
