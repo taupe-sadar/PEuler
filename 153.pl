e8d7e28bb9b16a94233929ac66abac6da2b320dd
Remi Bertin <remiber@free.fr>
2017-07-18 00:07:46 +0200

    Remove previous heuristics, that were used for testing

--- 153.pl
+++ 153.pl
@@ -1,35 +1,9 @@
 use strict;
 use warnings;
 use Data::Dumper;
-use Prime;
 use POSIX qw/floor ceil/;
-use List::Util qw( sum );
-
-use Divisors;
 
 my($max)=10**8;
-
-# Heuristic 0
-# Divisors::calc_all_until($max);
-# my($rlist)=Divisors::get_list();
-# print sum(@$rlist[1..$#$rlist])."\n";
-
-# print Dumper $rlist;
-
-# Heuristic 1
-# my($s)=0;
-# my($c)=1;
-# my($k)=1;
-# while($c>0)
-# {
-  # $c= floor($max/$k)*$k;
-  # $s += $c;
-  # $k++;
-# }
-
-# print "-> $s\n";
-
-# Heuristic 2
 
 my($s2)= calc_divs($max,10000);
 
