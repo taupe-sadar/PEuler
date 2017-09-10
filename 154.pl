d710d94406642b073942a1c246e6f56d66b89aa0
remi_from_bernin <remiber@free.fr>
2017-09-10 19:49:18 +0200

    Write test and fix num_triplets function

--- 154.pl
+++ 154.pl
@@ -1,6 +1,8 @@
 use strict;
 use warnings;
 use Data::Dumper;
+use List::Util qw( max min );
+
 
 my($p)=5;
 my($n)=200000;
@@ -8,6 +10,30 @@ my($n)=200000;
 my($ndecomposed)=dec_base( $n, $p );
 
 # my($target_div)=12;
+
+my(%hash)=();
+
+for(my($i)=0; $i < 1000; $i++ )
+{
+  my($rdec)=dec_base($i,10);
+ 
+  my($x)=0;
+  for( my($j)=0; $j<= $#$rdec ; $j++)
+  {
+    $x+= $$rdec[ $j ]; 
+  }
+  
+  $hash{$x} =  0 if(!exists($hash{$x}));
+  $hash{$x}++;
+} 
+
+
+
+for( my($i)=-3; $i <= 30 ; $i++ )
+{
+  $hash{$i} =  0 if(!exists($hash{$i}));
+  print "$i $hash{$i} ".num_triplets_terms($i,0,9)."\n";
+}
 
 sub list_divisors_pascal_pyramid
 {
@@ -18,7 +44,7 @@ sub list_divisors_pascal_pyramid
     my($max_carry)=($i==0)?0:2;
     for( my($k)=0; $k<= $max_carry; $k++ )
     {
-      my($max_carry_next)=($i==$#$ndec)?0:2;
+      my($max_carry_end)=($i==$#$ndec)?0:2;
       for( my($m)=0; $m<= $max_carry_end; $m++ )
       {
         my($num)= num_triplets_terms( $m * $base - $k, 0 , $base -1 );
@@ -34,11 +60,14 @@ sub num_triplets_terms
   my($l_min)=max($low,$n-2*$high);
   my($l_max)=min($high,$n-2*$low);
   
-  my($l0)=$n - $low - $high; #metter un min a s1 un un max a s2
+  my($l0)=$n - $low - $high;
+  my($l0_1)=min( $l0, $l_max );
+  my($l0_2)=max( $l0+1, $l_min );
+  
   
   my($num)=0;
-  $num+= max( 0, $l0 - $l_min + 1 ) * ($l_min - ($n - 2*$high) + $l0 -($n -2*$high) + 1  ) /2;
-  $num+= max( 0, $l_max - ($l0+1 ) * (($n - 2*$low) - $lmax + 1 + ($n -2*$low) - ($l0+1)+ 1  ) /2;
+  $num+= max( 0, $l0_1 - $l_min + 1 ) * ($l_min - ($n - 2*$high) + 1 +  $l0_1 -($n -2*$high) + 1  ) /2;
+  $num+= max( 0, $l_max - $l0_2 + 1 ) * (($n - 2*$low) - $l_max + 1 + ($n -2*$low) - $l0_2+ 1  ) /2;
   return $num # A tester
 }
 
@@ -55,7 +84,7 @@ sub dec_base
   return \@dec;
 }
 
-print_pascal(22);
+# print_pascal(22);
 
 sub print_pascal
 {
