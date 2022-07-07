92b952720106c089a55818d46e97d2fd4ae80a6d
Remi Bertin <remiber@free.fr>
2022-07-07 18:02:16 +0200

    Remove old traces

--- 155.pl
+++ 155.pl
@@ -41,14 +41,9 @@ for(my($i)=2;$i<=$n;$i++)
       for(my($a)=0;$a<= ($#{$values{$j}}); $a++)
       {
         my($valj)=$values{$j}[$a];
-        # print "Id : ".$$valj[1]."\n";<STDIN>;
         if($$valj[1] <= $$valk[2] )
         {
           my($sum_frac)=$$valk[0] + $$valj[0]->inverse();
-          # if( $sum_frac->numerator() < $sum_frac->denominator() )
-          # {
-          #   $sum_frac = $sum_frac->inverse();
-          # }
           my($key)=($sum_frac->numerator() >  $sum_frac->denominator())?Fraction::print_frac($sum_frac):Fraction::print_frac($sum_frac->inverse());
           
           if(exists($known{$key}))
@@ -57,7 +52,6 @@ for(my($i)=2;$i<=$n;$i++)
           }
           else
           {
-            # print "$key \n";
             push(@vals,[$sum_frac,$count++,$$valj[1]]);
             $news++;
             $known{$key} = 1;
@@ -65,17 +59,6 @@ for(my($i)=2;$i<=$n;$i++)
         }
         else
         {
-          # my($sum_frac)=$$valk[0] + $$valj[0]->inverse();
-          # my($key)=Fraction::print_frac($sum_frac);
-          # if(!exists($known{$key}))
-          # {
-            # print "About to skip(1) : $key : ".Fraction::print_frac($$valk[0])." + ".Fraction::print_frac($$valj[0]->inverse())."\n";
-            # print "$key : \n";
-            # print "  $$valj[1] - $$valj[2] ".print_obj(dual($$valj[3]))."\n" ;
-            # print "  $$valk[1] - $$valk[2] ".print_obj($$valk[3])."\n" ;
-            # <STDIN>;
-          # }
-          
           last;
         }
       }
@@ -85,10 +68,6 @@ for(my($i)=2;$i<=$n;$i++)
         if($$valj[1] <= $$valk[1] )
         {
           my($sum_frac)=$$valk[0]->inverse() + $$valj[0]->inverse();
-          # if( $sum_frac->numerator() < $sum_frac->denominator() )
-          # {
-          #   $sum_frac = $sum_frac->inverse();
-          # }
           my($key)=($sum_frac->numerator() >  $sum_frac->denominator())?Fraction::print_frac($sum_frac):Fraction::print_frac($sum_frac->inverse());
           if(exists($known{$key}))
           {
@@ -96,7 +75,6 @@ for(my($i)=2;$i<=$n;$i++)
           }
           else
           {
-            # print "$key \n";
             push(@vals,[$sum_frac,$count++,$$valj[1]]);
             $news++;
             $known{$key} = 1;
@@ -108,13 +86,11 @@ for(my($i)=2;$i<=$n;$i++)
         }
       }
     }
-    print "  $j + ".($i-$j)." : news : $news, discards : $discards\n"; #<STDIN>;
+    print "  $j + ".($i-$j)." : news : $news, discards : $discards\n";
   }
   
   $values{$i} = \@vals;
-  # <STDIN>;
   print "$i : ".($#vals+1)."\n";
-  # print Dumper \%vals;<STDIN>;  
 }
 
 sub dual
