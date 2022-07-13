85a48e96460430b55cbf42ed725c51ee1a0d54be
Remi Bertin <remiber@free.fr>
2022-07-13 10:57:30 +0200

    Final implementation, with break in main loop

--- 156.pl
+++ 156.pl
@@ -2,10 +2,12 @@ use strict;
 use warnings;
 use Data::Dumper;
 
-# print count_digits(400001,1)."\n";
-
-# exit(0);
-print "Total : ".seek_count_digits(1);
+my($final_sum)=0;
+for(my($d)=1;$d<=9;$d++)
+{
+  $final_sum += seek_count_digits($d);
+}
+print $final_sum;
 
 sub seek_count_digits
 {
@@ -13,14 +15,17 @@ sub seek_count_digits
   my($k)=0;
   my($sum)=0;
   my($range)=1;  
-    
-  while($k<=12)
+  my($crosses_diag)=1; 
+  while($crosses_diag)
   {
-    #Fenetre 10**k .. 10**k
-    $sum += recursive_seek($range,$range,$range*10 - 1,$digit);
+    #Fenetre 10**k .. 10**(k+1) - 1
+    my($s,$cd) = recursive_seek($range,$range,$range*10 - 1,$digit);
+    $sum +=$s;
+    $crosses_diag = $cd;
     $range*=10;
     $k++;
   }
+  print "($digit) Stop at (".($k-1).") : $sum\n";
   return $sum;
 }
 
@@ -31,22 +36,27 @@ sub recursive_seek
   my($fl)=count_digits($low,$d);
   my($fh)=count_digits($high,$d);
   
-   # print "($range) $low,$high -> $fl,$fh\n"; <STDIN>;
+  # print "($range) $low,$high -> $fl,$fh\n"; #<STDIN>;
   
-  if($fh < $low || $fl > $high)
+  if($fh < $low )
   {
-    return 0;
+    return (0,1);
+    
+  }
+  elsif( $fl > $high)
+  {
+    return (0,0);
   }
   else
   {
     my($sum)=0;
+    my($may_cross_diag)=0;
     my($start)=$low;
     if( $range == 1 )
     {
       if($fl == $low)
       {
         $sum += $fl;
-        print "$fl\n";
       }
       for(my($i)=1;$i<=8;$i++)
       {
@@ -54,33 +64,34 @@ sub recursive_seek
         if(count_digits($try,$d) == $try)
         {
           $sum += $try;
-          print "$try\n";
         }
       }
       if($fh == $high)
       {
         $sum += $fh;
-        print "$fh\n";
       }
+      $may_cross_diag = 1;
     }
     else
     {    
       my($reduce_range)=$range/10;
       for(my($i)=0;$i<=9;$i++)
       {
-        $sum += recursive_seek($reduce_range,$start,$start+$range-1,$d);
+        my($s,$mcd)=recursive_seek($reduce_range,$start,$start+$range-1,$d);
+        $sum += $s;
+        $may_cross_diag = 1 if($mcd == 1);
         $start += $range;
       }
     }
-    return $sum;
+    return ($sum,$may_cross_diag);
+    
   }
 }
-
-
 
 sub count_pow10
 {
   my($pow)=@_;
+  return 0 if($pow == 0);
   return 10**($pow-1)*$pow;
 }
 
@@ -97,6 +108,8 @@ sub count_digits
   my($val) = 0;
   $val += count_pow10($exp)*$head;
   $val += 10**$exp if($head > $d );
+  
+  
   
   if($suffix ne "" )
   {
