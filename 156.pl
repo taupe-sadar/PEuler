0f4574cf0b344a2fb72157e1b9c26a2418fed6ad
Remi Bertin <remiber@free.fr>
2022-07-13 10:16:50 +0200

    First simple valid implementation. We need a stop in the main loop

--- 156.pl
+++ 156.pl
@@ -2,7 +2,81 @@ use strict;
 use warnings;
 use Data::Dumper;
 
-print count_digits(10,1);
+# print count_digits(400001,1)."\n";
+
+# exit(0);
+print "Total : ".seek_count_digits(1);
+
+sub seek_count_digits
+{
+  my($digit)=@_;
+  my($k)=0;
+  my($sum)=0;
+  my($range)=1;  
+    
+  while($k<=12)
+  {
+    #Fenetre 10**k .. 10**k
+    $sum += recursive_seek($range,$range,$range*10 - 1,$digit);
+    $range*=10;
+    $k++;
+  }
+  return $sum;
+}
+
+sub recursive_seek
+{
+  my($range,$low,$high,$d)=@_;
+  
+  my($fl)=count_digits($low,$d);
+  my($fh)=count_digits($high,$d);
+  
+   # print "($range) $low,$high -> $fl,$fh\n"; <STDIN>;
+  
+  if($fh < $low || $fl > $high)
+  {
+    return 0;
+  }
+  else
+  {
+    my($sum)=0;
+    my($start)=$low;
+    if( $range == 1 )
+    {
+      if($fl == $low)
+      {
+        $sum += $fl;
+        print "$fl\n";
+      }
+      for(my($i)=1;$i<=8;$i++)
+      {
+        my($try)=$low+$i;
+        if(count_digits($try,$d) == $try)
+        {
+          $sum += $try;
+          print "$try\n";
+        }
+      }
+      if($fh == $high)
+      {
+        $sum += $fh;
+        print "$fh\n";
+      }
+    }
+    else
+    {    
+      my($reduce_range)=$range/10;
+      for(my($i)=0;$i<=9;$i++)
+      {
+        $sum += recursive_seek($reduce_range,$start,$start+$range-1,$d);
+        $start += $range;
+      }
+    }
+    return $sum;
+  }
+}
+
+
 
 sub count_pow10
 {
