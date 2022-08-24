9194f13eeaafdc01438d76962c45d4ae9a3a4f68
Remi Bertin <remiber@free.fr>
2022-08-24 23:58:16 +0200

    Introducing comment

---
+++ 166.pl
@@ -0,0 +1,20 @@
+use strict;
+use warnings;
+use Data::Dumper;
+
+# Variables names 
+#
+#   a  i  j  b
+#   k  e  f  l
+#   m  g  h  n
+#   c  o  p  d
+# 
+#   Sum : S
+#
+#   With some calculation we deduce than we have
+#   a + b = o + p (and other symetrical)
+#   a + d = g + f
+#   b + c = e + h
+#   a + b +c + d = e + f + g + h = S
+#
+
