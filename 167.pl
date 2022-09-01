fe59c0a527b63247eb3bceec9d792fe92692ebd4
Remi Bertin <remiber@free.fr>
2022-09-02 01:05:28 +0200

    Resolving 167, Ulam sequences, using research papers, and some calculations. Still need some comments, in order to explain

--- 167.pl
+++ 167.pl
@@ -3,29 +3,62 @@ use warnings;
 use Data::Dumper;
 
 use Hashtools;
+use integer;
 
-my(%all_sums)=();
-
-my(@seq)=(2,9);
-$all_sums{$seq[0] + $seq[1]}=1;
-
-my($n)=2;
-my($current)=$seq[1]+1;
-while($n < 500 )
+my($n)=10**11;
+my($count)=0;
+for(my($i)=2;$i<=10;$i++)
 {
-  if( exists($all_sums{$current}))
+  $count+= ulam_2_2k1(2*$i+1,$n-1);
+}
+print $count;
+
+sub ulam_2_2k1
+{
+  my($v,$idx)=@_;
+  
+  my($mask)=(1<<($v+1))-1;
+  my($state)=$mask;
+  my($ulam)=$v;
+  
+  my(@list)=();
+  
+  my($left)=$state & 0x1;
+  my($shift)=$state >> $v;
+  $state = (($state << 1) | ($left^$shift))& $mask;
+  if( $shift == 1 )
   {
-    if( $all_sums{$current} == 1 )
-    {
-      push(@seq,$current);
-      for(my($i)=0;$i<$#seq;$i++)
-      {
-        Hashtools::increment(\%all_sums,$current + $seq[$i]);
-      }
-      print "$n : $current (".($current-$seq[-2]).")\n"; 
-      $n++;
-    }
+    push(@list,$ulam);
   }
-  $current++;
+  $ulam += 2;
+
+  while($state != $mask)
+  {
+    $left = $state & 0x1;
+    $shift=$state >> $v;
+    $state = (($state << 1) | ($left^$shift))& $mask;
+    if( $shift == 1 )
+    {
+      push(@list,$ulam);
+    }
+    $ulam += 2;
+  }
+  my($period) = $ulam - $list[0];
+  my($num) = $#list + 1;
+  
+  if($idx == 0)
+  {
+    return 2;
+  }
+  elsif($idx == ($v+5)/2)
+  {
+    return 2 * $v + 2;
+  }
+  
+  #Counting the two even numbers
+  my( $fix ) = ($idx < $v)? -1 : -2;
+  
+  my($value)= ($idx/$num)*$period + $list[$idx%$num + $fix] ;
+  return $value;
 }
 
