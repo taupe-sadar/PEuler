a91daeab48ede14e5c5c555ce24d12f3d45c357c
Remi Bertin <remiber@free.fr>
2022-09-06 02:28:13 +0200

    Add comment for the binary representation

--- 169.pl
+++ 169.pl
@@ -3,8 +3,20 @@ use warnings;
 use Data::Dumper;
 use integer;
 
-print power2_ways((5**5)**5,25); #means 5^25 * 2^25
+# We consider the bianry representation of numbers. We can notice that
+# Long sequence of zeros may lead to several representations if two powers of 2 are allowed
+# Example : 10000 = 02000 = 01200 = 01120 = 01112
+#
+# Then, the quantity of representations depends of the series of zeros in a binary form of a numbers
+# Let us consider (a_i) the sequence of the continuous zeros.
+# Example : for n = 1000110100, a_i = (2,1,0,3)
+#
+# Then we can see that (f(n),g(n)) is equal to the product of the matrixes 
+#
+# M_i = | 1   a_i    |
+#       | 1  a_i + 1 |  , starting with (f(0),g(0))=(1,1)
 
+print power2_ways((5**5)**5,25); #means 5^25 * 2^25
 
 sub power2_ways
 {
