f00323f1d8d424a0e450ff0599f6d00a59222843
Remi Bertin <remiber@free.fr>
2022-11-25 00:46:50 +0100

    Add big comment about Z/pqZ explanation

--- 182.pl
+++ 182.pl
@@ -1,10 +1,47 @@
 use strict;
 use warnings;
 use Data::Dumper;
-use Gcd;
 use Prime;
 use Bezout;
 use List::Util qw( sum );
+
+# If p is prime, then (Z/pZ)* (all inversibles of Z/pZ) is isomorphic to (Z/(p-1)Z,+)
+# From left Chinois theorem, with p,q primes Z/pqZ is isomorphic to (Z/(p-1)Z x Z/(q-1)Z,+)
+#
+# 1) m is inversible in Z/pqZ 
+# Let m = x^a * y^b, with x,y, generators of order p-1 and q-1 in Z/pqZ
+# m^e = m <=> m^(e-1) = 1 <=> p-1 | (e-1)*a && q-1 | (e-1)*b
+# With d = (p-1)^(e-1), p-1 = d p', e-1 = d e'
+# We have p' | e' * a => p' | a, so it happens for a = 0, p', 2p', ..., (d-1)p', ie in d different ways.
+# The same goes for b, so the number of m such that m^e = m is 
+#  A = (p-1)^(e-1) * (q-1)^(e-1)
+#
+# 2) m is multiple of p. The multiples of p is a ring, it can be shown that it is isomorphic to Z/qZ.
+# With the same reasoning, with a generator x^a of order q-1, the number of m such that m^e = m is :
+#  B = (q-1)^(e-1)
+#
+# 3) m is multiple of q. Same as above, the number of m such that m^e = m is
+#  C = (p-1)^(e-1)
+#
+# 4) m is 0
+#
+# The overall number A + B + C + 1 = ((p-1)^(e-1) + 1) * ((q-1)^(e-1) + 1)
+#
+# We have also one constraint : e^phi = 1.
+# As phi is even, e is odd and e-1 is even, as p-1 and q-1.
+# Then the minimal reachable for (p-1)^(e-1) or (q-1)^(e-1) is 2.
+#
+# For counting those cases, we considerer the primes pi in phi's prime decomposition
+# For each pi we must have :
+# e-1 != 0 [pi]
+# e != 0 [pi] (first constraint)
+#
+# Exception for pi = 2
+# e-1 = 0 [2]
+# e-1 != 0 [4]
+# So e-1 = 2 [4]
+#
+# The algorithm solves the above Chinois problem, and count repeats over the prod(pi) period
 
 my($p,$q)=(1009,3643);
 my($n)=$p*$q;
@@ -44,37 +81,4 @@ my($num_periods)=$phi/$period;
 my($simple_sum)=sum(@$residus_e);
 
 my($sol)=$num_periods * $simple_sum + $period*($#$residus_e + 1)* ($num_periods - 1) * $num_periods/2;
-print "$sol\n";
-# my(@sorted)=sort({$a<=>$b} @$residus_e);
-# print Dumper [@sorted[0..20]];
-# <STDIN>;
-
-
-# print Dumper \%dec;
-
-my($min)=$n;
-my($sum)=0;
-for(my($e)=2;$e<0*$phi;$e++)
-{
-  my($count)=0;
-  my($gcd_e)=Gcd::pgcd($e,$phi);
-  next if($gcd_e > 1);
-  my($gcd_ep)=Gcd::pgcd($e-1,$p-1);
-  my($gcd_eq)=Gcd::pgcd($e-1,$q-1);
-  my($guess)=$gcd_ep* $gcd_eq + $gcd_ep +$gcd_eq;
-  
-  if($guess < $min)
-  {
-    $min = $guess;
-    $sum = 0;
-  }
-  if($guess == $min)
-  {
-    $sum += $e;
-    
-    # print "--> $e ($min)\n";<STDIN>;
-  }
-  
-  
-}
-# print "$min : $sum\n";
+print $sol;
