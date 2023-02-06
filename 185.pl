41bfcf4d352bfbe97c4cc3ff04b22be3b3c03662
Remi Bertin <remiber@free.fr>
2023-02-06 18:11:25 +0100

    Current work about number mind

---
+++ 185.pl
@@ -0,0 +1,266 @@
+use strict;
+use warnings;
+use Data::Dumper;
+
+my(@raw_grid)=(
+  [90342,2],
+  [70794,0],
+  [39458,2],
+  [34109,1],
+  [51545,2],
+  [12531,1]
+);
+
+my($init_state)=build_initial_state(\@raw_grid);
+
+
+
+my($state)=copy_state($init_state);
+
+my($error)=obvious_solve($state);
+print_state($state);
+
+
+sub print_state
+{
+  my($rstate)=@_;
+  print "--------\n";
+  print_board($rstate);
+  print "--------\n";
+  print join('',@{$$rstate{'solution'}})."\n";
+  print "--------\n";
+  my(@list)=();
+  my($max)=0;
+  for(my($j)=0;$j<$$rstate{'size'};$j++)
+  {
+    my(@ks)=sort(keys(%{$$rstate{'candidates'}[$j]}));
+    push(@list,\@ks);
+    $max = $#ks+1 if( $#ks + 1 > $max );
+  }
+  for(my($i)=0;$i<$max;$i++)
+  {
+    for(my($j)=0;$j<$$rstate{'size'};$j++)
+    {
+      if($#{$list[$j]} >= $i)
+      {
+        print $list[$j][$i];
+      }
+      else
+      {
+        print ' ';
+      }
+    }
+    print "\n";
+  }
+  print "--------\n";
+}
+
+
+sub print_board
+{
+  my($rstate)=@_;
+  my($rboard)=$$rstate{'board'};
+  for(my($i)=0;$i<=$#$rboard;$i++)
+  {
+    for(my($j)=0;$j<=$#{$$rboard[$i]{'digits'}};$j++)
+    {
+      print $$rboard[$i]{'digits'}[$j];
+    }
+    print "\n";
+  }
+  
+}
+
+sub obvious_solve
+{
+  my($rstate)=@_;
+  
+  my($has_changed)=1;
+  my($contradiction)=0;
+  while( $has_changed && !$contradiction)
+  {
+    $has_changed = 0;
+    my($rboard)=$$rstate{'board'};
+    for(my($i)=0;$i<=$#$rboard;$i++)
+    {
+      if($$rboard[$i]{'unknown'} > 0)
+      {
+        if($$rboard[$i]{'match'} == 0)
+        {
+          $has_changed = 1;
+          for(my($n)=0;$n<=$#{$$rboard[$i]{'digits'}};$n++)
+          {
+            my($digit)=$$rboard[$i]{'digits'}[$n];
+            if( $digit ne '.' )
+            {
+              delete $$rstate{'candidates'}[$n]{$digit};
+              $$rboard[$i]{'digits'}[$n] = '.';
+              $$rboard[$i]{'unknown'} --;
+            }
+          }
+          
+        }
+        elsif($$rboard[$i]{'match'} == $$rboard[$i]{'unknown'})
+        {
+          $has_changed = 1;
+          for(my($n)=0;$n<=$#{$$rboard[$i]{'digits'}};$n++)
+          {
+            my($digit)=$$rboard[$i]{'digits'}[$n];
+            if( $digit ne '.' )
+            {
+              $$rstate{'candidates'}[$n]{$digit} = {};
+              $$rstate{'solution'}[$n] = $digit;
+        
+              $$rboard[$i]{'digits'}[$n] = '.';
+              $$rboard[$i]{'unknown'} --;
+              $$rboard[$i]{'match'} --;
+            }
+          }
+        }
+      }
+    }
+    
+    # print_state($rstate); <STDIN>;
+    
+    for(my($j)=0;$j<$$rstate{'size'};$j++)
+    {
+      my($sol)=$$rstate{'solution'}[$j];
+      if( $sol ne '.' )
+      {
+        for(my($i)=0;$i<=$#$rboard;$i++)
+        {
+          if($$rboard[$i]{'unknown'} > 0 && $$rboard[$i]{'digits'}[$j] eq $sol)
+          {
+            $$rboard[$i]{'digits'}[$j] = '.';
+            $$rboard[$i]{'unknown'} --;
+            $$rboard[$i]{'match'} --;
+          }
+        }
+      }
+      else
+      {
+        my($rcands)=$$rstate{'candidates'}[$j];
+        
+        my(@ks)=(keys(%$rcands));
+        if( $#ks < 0 )
+        {
+          $contradiction = 1;
+          last;
+        }
+        else
+        {
+          my($sol)='.';
+          $sol = $ks[0] if( $#ks == 0 );
+          
+          for(my($i)=0;$i<=$#$rboard;$i++)
+          {
+            my($digit)=$$rboard[$i]{'digits'}[$j];
+            if($digit ne '.' )
+            {
+              if( $digit eq $sol )
+              {
+                $has_changed = 1;
+                $$rboard[$i]{'digits'}[$j] = '.';
+                $$rboard[$i]{'unknown'} --;
+                $$rboard[$i]{'match'} --;
+              }
+              elsif( !exists($$rcands{$digit} ))
+              {
+                $has_changed = 1;
+                $$rboard[$i]{'digits'}[$j] = '.';
+                $$rboard[$i]{'unknown'} --;
+              }
+            }
+          }
+        }
+      }
+    }
+    # print_state($rstate); <STDIN>;
+  }
+  return $contradiction;
+}
+
+
+sub build_initial_state
+{
+  my($rgrid)=@_;
+  my(%state)=();
+  my(@board)=();
+  
+  my($num_digits)=0;
+  
+  for( my($line)=0;$line <= $#$rgrid; $line ++)
+  {
+    my(@digits)=split('',$$rgrid[$line][0]);
+    $num_digits = $#digits + 1 if( $num_digits == 0 );
+    die "Uncoherent number of digits" if($num_digits != $#digits + 1);
+    
+    my(%hash)=(
+      'digits' => \@digits,
+      'match' => $$rgrid[$line][1],
+      'unknown' => $num_digits
+    );
+    push(@board,\%hash);
+  }
+  $state{'board'} = \@board;
+  
+  my(@solution)=();
+  for( my($i)=0; $i < $num_digits; $i++)
+  {
+    $solution[$i] = '.';
+  }
+  $state{'solution'} = \@solution;
+  
+  my(@candidates)=();
+  for( my($i)=0; $i < $num_digits; $i++)
+  {
+    my(%cands)=();
+    for( my($j)=0; $j < 10; $j++)
+    {
+      $cands{$j} = 1;
+    }
+    push(@candidates,\%cands);
+  }
+  $state{'candidates'} = \@candidates;
+  
+  $state{'size'} = $num_digits;
+  
+  return \%state;
+}
+
+sub copy_state
+{
+  my($rstate)=@_;
+  
+  my(%state)=();
+  
+  my(@board)=();
+  my(@solution)=();
+  my(@candidates)=();
+  
+  for( my($line)=0;$line <= $#{$$rstate{'board'}}; $line ++)
+  {
+    my(@digits)=(@{$$rstate{'board'}[$line]{'digits'}});
+    my(%hash)=(
+      'digits' => \@digits,
+      'match' => $$rstate{'board'}[$line]{'match'},
+      'unknown' => $$rstate{'board'}[$line]{'unknown'}
+    );
+    push(@board,\%hash);
+  }
+  $state{'board'} = \@board;
+  
+  @solution = (@{$$rstate{'solution'}});
+  $state{'solution'} = \@solution;
+  
+  for( my($i)=0; $i < $$rstate{'size'}; $i++)
+  {
+    my(%cands)=(%{$$rstate{'candidates'}[$i]});
+    push(@candidates,\%cands);
+  }
+  $state{'candidates'} = \@candidates;
+  
+  $state{'size'} = $$rstate{'size'};
+  
+  return \%state;
+}
