dd56bffa253b492659344080845dbad642863607
Remi Bertin <remiber@free.fr>
2023-08-12 01:14:36 +0200

    Easy solved 187 Add comment, archiving

--- 187.pl
+++ 187.pl
@@ -2,12 +2,36 @@ use strict;
 use warnings;
 use Data::Dumper;
 use Prime;
+use Sums;
 
-Prime::init_crible(10**8+1000);
-my($p)=Prime::next_prime();
-my($i)=1;
-while(($p=Prime::next_prime()) < 10**8)
+# Storing n primes (p_i) until sqrt(max) : thats n*(n+1)/2 semiprimes
+# Then, for each pi, there is prime bound (q_j) such that 
+#    q_j * p_i < max, and q_(j+1) * p_i > max
+#    that counts for i+1 *(number of primes in [q-j-1 , q_j]
+
+my($max)=10**8;
+Prime::init_crible($max+1000);
+my(@primes)=();
+my($p)=0;
+my($middle)=sqrt($max);
+while(($p=Prime::next_prime()) < $middle)
 {
-  $i++;
-  print "$i : $p\n" if($i%1000 == 0);
+  push(@primes,$p);
 }
+
+my($count)=Sums::int_sum($#primes+1);
+my($current_idx)=$#primes;
+
+while($current_idx>=0)
+{
+  my($next_bound)=$max/$primes[$current_idx];
+  while( $p < $next_bound)
+  {
+    $count += $current_idx+1;
+    $p = Prime::next_prime();
+  }
+  
+  $current_idx--;
+}
+
+print $count;
