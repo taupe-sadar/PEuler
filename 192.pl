ebdd7ef6efe8d238b79f071736b2cf56c0647a24
Remi Bertin <remiber@free.fr>
2023-09-19 02:25:49 +0200

    Solved Best approx using continued fracs. Need comment and archiving. Slow for some reason

--- 192.pl
+++ 192.pl
@@ -4,157 +4,93 @@ use warnings;
 use Data::Dumper;
 use POSIX qw/floor ceil/;
 use List::Util qw/min max/;
+use ContinueFraction;
 
-for(my($n)=1;$n<100;$n++)
+my($denom_max)=10**12;
+my($max_n)=10**5;
+
+my($sum_q)=0;
+
+for(my($n)=1;$n<=$max_n;$n++)
 {
   my($squ)=sqrt($n);
   my($isqu)=int($squ);
   next if($isqu*$isqu == $n);
 
-  my(@alist)=();
-  my($rep)=[1,0,1];
-  while($$rep[0]!=1 || $$rep[1]!=$isqu || $$rep[2]!=1)
+  my(@clist)=ContinueFraction::integers_list($n);
+  my(@qns)=(0,1);
+  my($i)=0;
+  my($a)=coef_frac(\@clist,$i);
+  my($q)=$a * $qns[0] + $qns[1];
+  while($q <= $denom_max )
   {
-    my($intpart)=int_eval(@$rep,$squ);
-    push(@alist,$intpart);
-    $$rep[1]-=$intpart*$$rep[2];
-    
-    my($a,$b,$c)=($$rep[2]*$$rep[0],-$$rep[2]*$$rep[1],$$rep[0]*$$rep[0]*$n - $$rep[1]*$$rep[1]);
-    my($d)=pgcd(pgcd($a,$b),$c);
-    # print "abcd : $a $b $c $d\n";<STDIN>;
-    $rep =[$a/$d,$b/$d,$c/$d];
-    
-    # print Dumper $rep;
-    # <STDIN>;
+    @qns = ($q,$qns[0]);
+    $i++;
+    $a=coef_frac(\@clist,$i);
+    $q = $a * $qns[0] + $qns[1];
   }
-  push(@alist,int_eval(@$rep,$squ));
-  print "------ n = $n -------\n";
-  print "$n : [".join(" ",@alist)."]\n";
-  my($limit)=1000;
-  my($rbest_approx_huygens,$rbest_approx_lagrange)=best_approxs($squ,$limit);
-  my($reduced,$secondary_reduced)=reduced_frac(\@alist,$limit);
-  print "---------------------------\n";
-  print "Reduced : ".string_array_fracs($reduced)."\n";
-  print "Secondary : ".string_array_fracs($secondary_reduced)."\n";
-  print "---------------------------\n";
-  print "Huygens : ".string_array_fracs($rbest_approx_huygens)."\n";
-  print "Lagrange : ".string_array_fracs($rbest_approx_lagrange)."\n";
-  <STDIN>;
-}
-
-sub best_approxs
-{
-  my($irr,$limit)=@_;
-  my($q)=1;
-
-  my($min_lagrange)=1000;
-  my(@approx_lagrange)=();
-
-  my($min_huygens)=1000;
-  my(@approx_huygens)=();
-
-  while($q <= $limit)
-  {
-    my($qirr)=$q*$irr;
-    my($best_p_lagrange)=0;
-    my($p)=ceil(-$min_lagrange + $qirr);
-    while( $p <= floor($min_lagrange + $qirr))
-    {
-      if(pgcd($p,$q)>1)
-      {
-        $p++;
-        next;
-      }
-      $best_p_lagrange = $p;
-      $min_lagrange = abs($p - $qirr);
-      $p = max(ceil(-$min_lagrange + $qirr),$p+1);
-    }
-    push(@approx_lagrange,[$best_p_lagrange,$q]) if($best_p_lagrange > 0);
-
-    
-    
-    my($best_p_huygens) = 0;
-    $p=ceil(-$min_huygens*$q + $qirr);
-
-    while( $p <= floor($min_huygens*$q + $qirr))
-    {
-      if(pgcd($p,$q)>1)
-      {
-        $p++;
-        next;
-      }
-      $best_p_huygens = $p;
-      $min_huygens = abs($p/$q - $irr);
-      $p = max(ceil(-$min_huygens*$q + $qirr),$p+1);
-    }
-    push(@approx_huygens,[$best_p_huygens,$q]) if($best_p_huygens > 0 && $best_p_huygens != $best_p_lagrange);
-
-    $q++;
-  }
-  return (\@approx_huygens,\@approx_lagrange);
-}
-
-sub reduced_frac
-{
-  my($ralist,$limit)=@_;
   
-  my($p,$pp)=(1,0);
-  my($q,$qq)=(0,1);
-  my(@reduced)=();
-  my(@secondary_reduced)=();
-
-  my($idx)=0;
-  while($q<=$limit)
+  $a=coef_frac(\@clist,$i);
+  my($b)=floor(($denom_max-$qns[1])/$qns[0]);
+  my($ret)=0;
+  my($secondary)=$b * $qns[0] + $qns[1];
+  if(2*$b < $a )
   {
-    my($a)=$$ralist[$idx];
-    my($pn,$qn)=($pp,$qq);
-    for(my($b)=1;$b < $a;$b++)
+    $ret = $qns[0];
+  }
+  elsif( 2*$b > $a )
+  {
+    $ret = $secondary;
+  }
+  else
+  {
+    my($sign)=1;
+    my($irr_idx)=$i+1;
+    my($q_idx)=$i-1;
+    while($q_idx > 0 )
     {
-      $pn+=$p;
-      $qn+=$q;
-      last if($qn > $limit);
-      push(@secondary_reduced,[$pn,$qn]);
+      my($irr_coeff) = coef_frac(\@clist,$irr_idx);
+      my($q_coeff) = coef_frac(\@clist,$q_idx);
+      if($irr_coeff != $q_coeff )
+      {
+        if( $sign * ($irr_coeff - $q_coeff ) > 0 )
+        {
+          $ret = $secondary;
+        }
+        else
+        {
+          $ret = $qns[0];
+        }
+        last;
+      }
+      $sign = -$sign;
+      $irr_idx++;
+      $q_idx--;
     }
-    $pn+=$p;
-    $qn+=$q;
-    last if($qn > $limit);
-    push(@reduced,[$pn,$qn]);
-    $idx++;
-    $idx = 1 if($idx > $#$ralist);
-    ($p,$pp)=($pn,$p);
-    ($q,$qq)=($qn,$q);
+    if( $q_idx == 0 )
+    {
+      if($sign < 0 )
+      {
+        $ret = $secondary;
+      }
+      else
+      {
+        $ret = $qns[0];
+      }
+    }
   }
-  return (\@reduced,\@secondary_reduced);
-}
-
-sub string_array_fracs
-{
-  my($rarray)=@_;
-  my(@frac_strings)= map {$$_[0]."/".$$_[1]} @$rarray;
-  return join(", ",@frac_strings);
-}
-
-sub int_eval
-{
-  my($a,$b,$c,$sqn)=@_;
-  return floor(($a*$sqn + $b)/$c);
-}
-
-sub pgcd
-{
-  my($a,$b)=@_;
-  return 0 if($a==0);
-  return 0 if($b==0);
-  ($a,$b)=($b,$a) if($b>$a);
-  $a = -$a if($a<0);
-  $b = -$b if($b<0);
   
-  while($a>0)
-  {
-    ($b,$a) = ($a,$b%$a);
-  }
-  return $b;
+  $sum_q += $ret;
+  
 }
 
+print $sum_q;
 
-
+sub coef_frac
+{
+  my($alist,$i)=@_;
+  my($aidx)=$i%($#$alist+1);
+  my($a)=$$alist[$aidx];
+  return $a if($i==0 || $aidx > 0);
+  return $a*2;
+}
