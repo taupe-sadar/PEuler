a8a1a4bfa1367f3ed2250dafe59f84bfc3043796
Remi Bertin <remiber@free.fr>
2023-09-30 19:02:59 +0200

    Add comment and archiving

--- 193.pl
+++ 193.pl
@@ -4,6 +4,23 @@ use warnings;
 use Data::Dumper;
 use POSIX qw/floor ceil/;
 
+# Instead of counting all number with no square until (n),
+# we count all numbers containing square divisors (m^2). Counting all (m) until sqrt(n)
+# If m = p is prime, there is floor(n/p^2) numbers multiple of p^2
+# But considering m = (p1 and p2), primes there are floor(n/p1^2) + floor(n/p1^2) - floor(n/p1^2)
+# that are multiple of p1 or p2
+# More generally we must consider all integers in [1;floor(sqrt(n))], which are square free, ie
+# m = p1* ..* pk
+# And count (-1)^(k+1) * floor(n/m^2)
+#
+# For knowing that value (-1)^(k+1) we use crible whiche values are : 
+# -1 : its a square dont use it
+#  0 : its a prime : increment all its multiples
+#  k : its a number divisible by k non square primes
+#
+# Note : there is an optimisation : the crible contains only numbers of the form 6*n +1 and 6*n+5
+# It speeds up the algorithms. Multiples of 2 3 6, are treated seperately
+
 my($max)=2**50;
 my($max_sq2)=floor(sqrt($max));
 
