7c2399e983e455f28748a4d4a0b2ce286ea81bab
Remi Bertin <remiber@free.fr>
2023-12-30 17:00:12 +0100

    Add comment, archiving

--- 198.pl
+++ 198.pl
@@ -3,6 +3,28 @@ use strict;
 use warnings;
 use Data::Dumper;
 
+# We use continued fraction notations as in the 192 problem.
+# the continued fraction of x can be written [a0, a1, a2, ... ]
+# With a_0 = int(x) , x = x_0 = a_0 + x_1
+#      a_n = int(1/x_n), x_n = a_n + x_n+1
+#
+# The reduced fraction are defined : p_n / q_n with
+# p_-1 = 1 , p_-2 = 0, p_n = p_n-1 * a_n + p_n-2
+# q_-1 = 0 , q_-2 = 1, q_n = q_n-1 * a_n + q_n-2
+#
+# For rationals, the continued fraction is finite : 
+# p/q = [a0,...., an]
+#
+# A best approximation (p/q = [a0,...,a_n-1,b]) of x verifies : 
+# [ a_n, a_n+1, ... ] < [ 2b, a_n-1, ..., a1 ]
+#
+# Ambiguous numbers are those x were the previous inequality is an equality instead.
+# then the ambiguous number is : 
+#   [a0,a1,...,a_n-1, an = 2*b, an_-1,...,a1]
+# And the best approximations are :
+#   [a0,...,a_n-1,b] and [a0,...,a_n-1]
+
+
 my($max_denom)=10**8;
 
 my($a1)=100;
