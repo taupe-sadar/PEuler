4cab7ce2348f12fc298d15cead62f82a737c02a5
Remi Bertin <remiber@free.fr>
2024-01-14 21:05:58 +0100

    Fix algorithm, by testing with example pretty print use generic number instead of 100 / 50

--- 201.pl
+++ 201.pl
@@ -5,14 +5,20 @@ use Data::Dumper;
 use Hashtools;
 use List::Util qw(max min);
 
+my($num_elts)=100;
+my($sub_num_elts)=$num_elts/2;
+
 my(@all_sets_values)=({0=>1});
 
-for(my($n)=1;$n<=100;$n++)
+my(@example)=(0,1,3,6,8,10,11);
+
+for(my($n)=1;$n<=$num_elts;$n++)
 {
   my($square)=$n*$n;
+  # my($square)=$example[$n];
   push(@all_sets_values,{}) if($n <= 50);
   print "--- $n ---\n";
-  for(my($set_size)=min($n,50)-1;$set_size>=max(0,$n-50);$set_size--)
+  for(my($set_size)=min($n,$sub_num_elts)-1;$set_size>=max(0,$n-$sub_num_elts-1);$set_size--)
   {
     my($rprev_values)=$all_sets_values[$set_size];
     foreach my $v (keys(%$rprev_values))
@@ -29,15 +35,22 @@ for(my($n)=1;$n<=100;$n++)
 
 my($sum)=0;
 my(@all)=();
-foreach my $v (keys(%{$all_sets_values[50]}))
+foreach my $v (keys(%{$all_sets_values[$sub_num_elts]}))
 {
-  if($all_sets_values[50]{$v} == 1)
+  if($all_sets_values[$sub_num_elts]{$v} == 1)
   {
     push(@all,$v);
     $sum+=$v;
   }
 }
+# print Dumper \@all_sets_values;
+
 @all=sort({$a<=>$b}@all);
-print Dumper \@all;
+# print Dumper \@all;
 print "# $#all\n";
+
+# Half : 
+# #339 / 679
+# 15016516 / 115039000
+
 print $sum;
