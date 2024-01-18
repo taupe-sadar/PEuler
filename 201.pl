fba4582f2871fd2fe12523b18c07ba6aaeca207d
Remi Bertin <remiber@free.fr>
2024-01-18 02:33:01 +0100

    Trying, weird mapping for approaching bound faster, but that doesnt work

--- 201.pl
+++ 201.pl
@@ -4,6 +4,7 @@ use warnings;
 use Data::Dumper;
 use Hashtools;
 use List::Util qw(max min);
+use Sums;
 
 my($num_elts)=100;
 my($sub_num_elts)=$num_elts/2;
@@ -13,28 +14,53 @@ my(@all_sets_details)=({0=>[[]]});
 
 my(@example)=(0,1,3,6,8,10,11);
 
-for(my($n)=100;$n>=1;$n--)
+my($maximum_bound)=Sums::int_square_sum($num_elts)/2;
+
+for(my($n)=$num_elts;$n>=1;$n--)
 {
   my($max_set)=$num_elts-$n+1;
+  
   # my($square)=$max_set*$max_set;
-  my($square)=$n*$n;
+  # my($square)=$n*$n;
+  
+  my($square)=$maping*$maping;
+  
+  
   # my($square)=$example[$n];
   push(@all_sets_values,{}) if($max_set <= $sub_num_elts);
-  push(@all_sets_details,{}) if($max_set <= $sub_num_elts);
+  # push(@all_sets_details,{}) if($max_set <= $sub_num_elts);
   print "--- $n ---\n";
   for(my($set_size)=min($max_set,$sub_num_elts)-1;$set_size>=max(0,$max_set-$sub_num_elts-1);$set_size--)
   {
     my($rprev_values)=$all_sets_values[$set_size];
     foreach my $v (keys(%$rprev_values))
     {
-      my($sum)=$v + $square;
-      next if($sum >= 46000);
-      # $all_sets_values[$set_size+1]{$sum}+=$$rprev_values{$v};
-      Hashtools::increment($all_sets_values[$set_size+1],$sum, $$rprev_values{$v});
+      # if( $v > $maximum_bound)
+      # {
+        # delete $$rprev_values{$v};
+      # }
+      # else
+      {
+        my($sum)=$v + $square;
+        next if($sum > $maximum_bound);
+        # next if($sum > 47050);
+        # $all_sets_values[$set_size+1]{$sum}+=$$rprev_values{$v};
+        Hashtools::increment($all_sets_values[$set_size+1],$sum, $$rprev_values{$v});
+      }
     }
+    
+    if($set_size == $sub_num_elts - 1)
+    {
+      while(exists($all_sets_values[$sub_num_elts]{$maximum_bound}) && $all_sets_values[$sub_num_elts]{$maximum_bound} >= 2)
+      {
+        $maximum_bound--;
+      }
+      print "Max bound : $maximum_bound\n";
+    }
+
     next;
-    
-    
+
+    #detailed;
     my($rprev_detailed)=$all_sets_details[$set_size];
     foreach my $v (keys(%$rprev_detailed))
     {
@@ -74,9 +100,9 @@ foreach my $v (keys(%{$all_sets_values[$sub_num_elts]}))
 # print Dumper \@all;
 print "# $#all\n";
 
-foreach my $v (sort({$a<=>$b} (keys(%{$all_sets_values[$sub_num_elts]}))))
-{
-  print "$v : => $all_sets_values[$sub_num_elts]{$v}\n";
+# foreach my $v (sort({$a<=>$b} (keys(%{$all_sets_values[$sub_num_elts]}))))
+# {
+  # print "$v : => $all_sets_values[$sub_num_elts]{$v}\n";
   # if(exists($all_sets_details[$sub_num_elts]{$v}))
   # {
     # my($rall)=$all_sets_details[$sub_num_elts]{$v};
@@ -86,7 +112,7 @@ foreach my $v (sort({$a<=>$b} (keys(%{$all_sets_values[$sub_num_elts]}))))
     # }
   # }
   # <STDIN>;
-}
+# }
 
 # Half : 
 # #339 / 679
