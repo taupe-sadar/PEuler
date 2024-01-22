b9ce0977c21b71aac7b9d7df25a382c9eb4edaaa
Remi Bertin <remiber@free.fr>
2024-01-22 23:46:47 +0100

    final implementation, using scheme for having a relevent bound

--- 201.pl
+++ 201.pl
@@ -9,33 +9,61 @@ use Sums;
 my($num_elts)=100;
 my($sub_num_elts)=$num_elts/2;
 
+my($maximum_bound)=0;
+
 if( 1 )
 {
-  my($first)=1;
-  my($last)=6;
-  
-  my($mid)=($last + $first -1)/2;
-  my($middle)=int((offset_square_sum($mid+1,$last)+offset_square_sum($first,$mid))/2);
-  my($final)=all_sums($first,$last);
-  
-  foreach my $k (sort({$a<=>$b}keys(%$final)))
+  my($subset_set)=1;
+  while(1)
   {
-    my($delta)=$k - $middle;
-    print "$delta -> $$final{$k}\n";
+    my($middle) = int((offset_square_sum($subset_set+1,$subset_set*2)+offset_square_sum(1,$subset_set))/2);
+    my($final)=all_sums(1,$subset_set*2);
+
+    # print_set($final,0);
+    my($min_twice)=$middle;
+    $min_twice-- while($$final{$min_twice});
+    my($max_twice)=$middle;
+    $max_twice++ while($$final{$max_twice});
+    
+    print "$subset_set : [$min_twice $max_twice]\n";
+    # <STDIN>;
+    if($max_twice - $min_twice > ($num_elts *2 + 1))
+    {
+      print "[$min_twice $max_twice]\n";
+      print offset_square_sum($subset_set*2 + 1,$num_elts/2+$subset_set);
+      $maximum_bound = offset_square_sum($subset_set*2 + 1,$num_elts/2+$subset_set) + $min_twice;
+      last;
+    }
+    $subset_set++;
   }
-  
-  # print Dumper $final;
-  exit( 0 );
+
+  # exit( 0 );
 }
 
 
+
+sub print_set
+{
+  my($s,$m)=@_;
+  foreach my $k (sort({$a<=>$b}keys(%$s)))
+    {
+      my($delta)=$k - $m;
+      print "$delta -> $$s{$k}\n";
+    }
+}
+
+my(@bound_for_set)=(0,1);
+for(my($i)=2;$i<=$sub_num_elts;$i++)
+{
+  push(@bound_for_set,$bound_for_set[-1],$i*$i);
+}
 
 my(@all_sets_values)=({0=>1});
 my(@all_sets_details)=({0=>[[]]});
 
 my(@example)=(0,1,3,6,8,10,11);
 
-my($maximum_bound)=Sums::int_square_sum($num_elts)/2;
+my($count)=0;
 
 for(my($n)=$num_elts;$n>=1;$n--)
 {
@@ -54,19 +82,21 @@ for(my($n)=$num_elts;$n>=1;$n--)
   for(my($set_size)=min($max_set,$sub_num_elts)-1;$set_size>=max(0,$max_set-$sub_num_elts-1);$set_size--)
   {
     my($rprev_values)=$all_sets_values[$set_size];
+    # my($corrected_bound)=$maximum_bound - $bound_for_set[$sub_num_elts - $set_size - 1];
     foreach my $v (keys(%$rprev_values))
     {
-      # if( $v > $maximum_bound)
-      # {
-        # delete $$rprev_values{$v};
-      # }
-      # else
+      if( $v > $maximum_bound)
+      {
+        delete $$rprev_values{$v};
+      }
+      else
       {
         my($sum)=$v + $square;
         next if($sum > $maximum_bound);
         # next if($sum > 47050);
         # $all_sets_values[$set_size+1]{$sum}+=$$rprev_values{$v};
         Hashtools::increment($all_sets_values[$set_size+1],$sum, $$rprev_values{$v});
+        # $count++;
       }
     }
     
@@ -120,6 +150,9 @@ foreach my $v (keys(%{$all_sets_values[$sub_num_elts]}))
 @all=sort({$a<=>$b}@all);
 # print Dumper \@all;
 print "# $#all\n";
+print "Complex : $count\n";
+#11634272
+#11772220
 
 # foreach my $v (sort({$a<=>$b} (keys(%{$all_sets_values[$sub_num_elts]}))))
 # {
@@ -139,7 +172,10 @@ print "# $#all\n";
 # #339 / 679
 # 15016516 / 115039000
 
-print $sum;
+my($all_sum)=offset_square_sum(1,$num_elts);
+
+print "$sum\n";
+print "".(($#all+1)*$all_sum)."\n";
 
 sub all_sums
 {
@@ -176,6 +212,5 @@ sub offset_square_sum
     # print "$i² ";
   # }
   # print "\n";
-  
-  return ($end - $start + 1) * ((2*$end + 1)*($start+$end) + 2*$start*($start-1))/6;
+  return Sums::int_square_sum($end) - Sums::int_square_sum($start-1);
 }
