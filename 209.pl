7a42e37cec8850ee0eea31cba2ba53d4ddfd6110
Remi Bertin <remiber@free.fr>
2024-02-21 01:17:07 +0100

    Add comment and archiving

--- 209.pl
+++ 209.pl
@@ -5,6 +5,23 @@ use Data::Dumper;
 use POSIX qw/floor/;
 use Permutations;
 
+# The function f: (a,b,c,d,e,f) -> (b,c,d,e,f,a^(b&c)) is bijective.
+# The set E of all values of (a,b,c,d,e,f) can be separated of 
+# of distincts orbits of f. In any orbit, each value x verify f^n(x)=x for some n > 0
+#
+# A truth table function T must verify that an element x in an orbit, and its successor f(x)
+# cant be both equal to 1. That is to say x AND f(x) = 0
+#
+# In order to count all those possibilities h(m) in an orbit of size (m), we consider all the ways to place some pieces of width 2
+# (representing a value and its successor), in a larger set of size (m), that set is wrapping, so a piece could
+# be placed on the begining and the end.
+#
+# So we have h(m) = sum_(k)( C(m-k,k) ) (k in 0..floor(m/2))     (no piece across the wrap)
+#                 + sum_(k)( C(m-k-1,k-1) ) (k in 1..floor(m/2)) (one piece across the wrap)
+#
+# Finally the amount of T functions are 
+# prod( h(m) ) for all orbits m of f.
+
 my($n)=6;
 
 my(@orbs)=analyze_orbits($n);
