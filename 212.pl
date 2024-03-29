edf2623005c78312eb68728257e26769d3f79770
Remi Bertin <remiber@free.fr>
2024-03-29 02:19:52 +0100

    Started 212.
    Algo done, but still a few bugs, when using divisions.
    The divisions are needed for large scale

--- 212.pl
+++ 212.pl
@@ -2,13 +2,22 @@
 use strict;
 use warnings;
 use Data::Dumper;
+use POSIX qw/floor/;
+use List::Util qw(max min);
 
-my($num_cuboids)=50000;
+my($num_cuboids)=100;
 my($space)=10000;
 my($width)=399;
-my($divisions)=25;
+my($divisions)=2;
 
 my(@cuboids)=();
+my(@regions)=();
+for(my($r)=0; $r < ($divisions**3); $r++)
+{
+  push(@regions,[]);
+}
+
+my($gen)=generator();
 
 for(my($c)=0;$c<$num_cuboids;$c++)
 {
@@ -16,22 +25,116 @@ for(my($c)=0;$c<$num_cuboids;$c++)
   my(@xidx)=(idx_div($$cubo[0]),idx_div($$cubo[0]+$$cubo[3]-1));
   my(@yidx)=(idx_div($$cubo[1]),idx_div($$cubo[1]+$$cubo[4]-1));
   my(@zidx)=(idx_div($$cubo[2]),idx_div($$cubo[2]+$$cubo[5]-1));
-  push(@cuboids,$cubo);
+  pop(@xidx) if($xidx[1]==$xidx[0]);
+  pop(@yidx) if($yidx[1]==$yidx[0]);
+  pop(@zidx) if($zidx[1]==$zidx[0]);
+
+  my(@cubo_idxs)=();
+  
+  for my $x (@xidx){ for my $y (@yidx){ for my $z (@zidx){
+    push(@cubo_idxs,region_id($x,$y,$z,$divisions));
+  }}}
+  shift(@cubo_idxs);
+  
+  push(@cuboids,[$cubo,\@cubo_idxs]);
+  my($rid)=region_id($xidx[0],$yidx[0],$zidx[0],$divisions);
+
+  push(@{$regions[$rid]},$c);
 }
 
 
-my($gen)=generator();
+my($union)=0;
+for(my($r)=0;$r<=$#regions;$r++)
+{
+  my($current_cuboids)=$regions[$r];
+  for(my($c)=0;$c<=$#$current_cuboids;$c++)
+  {
+    $union += volume($cuboids[$c][0]);
+    
+    my(@all_others)=();
+    for(my($cc)=$c+1;$cc<=$#$current_cuboids;$cc++)
+    {
+      push(@all_others,$cuboids[$cc][0]);
+    }
+    for(my($idx_adj)=0;$idx_adj<=$#{$cuboids[$c][1]};$idx_adj++)
+    {
+      my($zone)=$cuboids[$c][1][$idx_adj];
+      for(my($cub_adj)=0;$cub_adj<=$#{$regions[$zone]};$cub_adj++)
+      {
+        my($cubo)=$cuboids[$regions[$zone][$cub_adj]][0];
+        
+        push(@all_others,$cubo);
+      }
+    }
+    $union -= rec_intersections($cuboids[$c][0],\@all_others);
+  }
+  
+  # print "($r) [".(join(" ",@{$regions[$r]}))."]\n";
+}
 
-my($c1)=cuboid($gen);
-my($c2)=cuboid($gen);
+print $union;
 
-print Dumper $c1;
-print Dumper $c2;
+
+sub rec_intersections
+{
+  my($current_intersected,$alls)=@_;
+  my($total_volume)=0;
+  for(my($i)=0;$i<=$#$alls;$i++)
+  {
+    my($inter)=intersection($current_intersected,$$alls[$i]);
+    my($vol)=volume($inter);
+    next if($vol==0);
+    $total_volume += $vol;
+    
+    if( $i < $#$alls )
+    {
+      my(@next_volumes)=@$alls[$i+1..$#$alls];
+      
+      $total_volume -= rec_intersections($inter,\@next_volumes);
+    }
+  }
+  return $total_volume;
+}
+
+sub intersection
+{
+  my($c1,$c2)=@_;
+  
+  my(@cr)=(0)x6;
+  for(my($co)=0;$co<3;$co++)
+  {
+    my($cmin,$cmax)=($$c1[$co]<$$c2[$co])?($c1,$c2):($c2,$c1);
+    
+    next if($$cmin[$co] + $$cmin[$co+3] <= $$cmax[$co]);
+    $cr[$co]=$$cmax[$co];
+    $cr[$co+3]=min($$cmax[$co+3], $$cmin[$co] - $$cmax[$co] + $$cmin[$co+3]);
+  }
+  # unless ($cr[3] == 0 || $cr[4] == 0 || $cr[5] == 0 )
+  # {
+    # print Dumper $c1;
+    # print Dumper $c2;
+    # print Dumper \@cr;
+    # <STDIN>;
+  # }
+  return \@cr;
+}
+
+sub volume
+{
+  my($cuboid)=@_;
+  return $$cuboid[3]*$$cuboid[4]*$$cuboid[5];
+}
+
+sub region_id
+{
+  my($x,$y,$z,$div)=@_;
+  return (($z*$div + $y)*$div + $x);
+}
 
 sub idx_div
 {
   my($coord)=@_;
-  return $coord*$divisions/($space+$width);
+  return floor($coord*$divisions/($space+$width));
 }
 
 
@@ -71,7 +174,7 @@ sub gen_next
   {
     my($kidx)=($k-1)%55;
     my($other_idx)=($kidx-24+55)%55;
-    $$generator[1][$kidx] += $$generator[1][$other_idx];
+    $$generator[1][$kidx] = ($$generator[1][$kidx] + $$generator[1][$other_idx]) %1000000;
     return $$generator[1][$kidx];
   }
 }
