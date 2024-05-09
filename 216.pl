e477674cc6a5c23a0f283eb56980ac4a473d2bfe
Remi Bertin <remiber@free.fr>
2024-05-10 00:11:28 +0200

    New algo, 50% gain, without Lengendre criteria, and using the fact that a new number in the crible is always a prime

--- 216.pl
+++ 216.pl
@@ -10,49 +10,61 @@ my($n)=50000000;
 
 my($sq2)=sqrt(2);
 my($prime_limit)=$n*$sq2;
-Prime::init_crible($prime_limit+1000);
 
-Prime::next_prime();
-my($p)=Prime::next_prime();
-my(@primes)=();
-my(%prime_infos)=();
-while($p < $prime_limit)
+if(0) #old prime search
 {
-  my($sm)= smart_mult_modulo(2,($p-1)/2,$p);
-  
-  if( $sm == 1 )
+  Prime::init_crible($prime_limit+1000);
+
+  Prime::next_prime();
+  my($p)=Prime::next_prime();
+  my(@primes)=();
+  my(%prime_infos)=();
+  while($p < $prime_limit)
   {
-    push(@primes,$p);
-    $prime_infos{$p}=0;
+    my($sm)= smart_mult_modulo(2,($p-1)/2,$p);
+    
+    if( $sm == 1 )
+    {
+      push(@primes,$p);
+      $prime_infos{$p}=0;
+    }
+    $p=Prime::next_prime();
   }
-  $p=Prime::next_prime();
+  print "Primes : $#primes\n";
+  print ("[".join(" ", @primes[0..20])."]\n");
+  # exit(0);
 }
-print "Primes : $#primes\n";
-print ("[".join(" ", @primes[0..20])."]\n");
-# exit(0);
+
 my(@crible_residuals)=(0)x($n+1);
 my($count_seen_primes)=0;
-for(my($i)=0;$i<=$#primes;$i++)
+for(my($i)=2;$i<=$n;$i++)
 {
-  my($pr)=$primes[$i];
   
   # print "prime : $pr\n";
   
-  my($residual)=0;
-  if($prime_infos{$pr}==0)
+  if($crible_residuals[$i]!=1)
   {
-    print "Search residuals for $pr ...\n";
-    $residual = find_residual($pr,($pr+1)/2);
-    die "Could not find root of ".(($pr+1)/2)." % $pr\n" if($residual == 0);
-  }
-  else
-  {
-    $residual = $prime_infos{$pr};
-  }
-  
-  foreach my $resi ($residual,$pr - $residual)
-  {
-    $count_seen_primes += init_residuals(\@crible_residuals,$pr,$resi,\%prime_infos);
+    my($prime)=0;
+    if($crible_residuals[$i]==0)
+    {
+      $count_seen_primes++;
+      $crible_residuals[$i] = 1;
+      $prime = f($i);
+      # print "($prime)\n";<STDIN>;
+    }
+    else
+    {
+      $prime = $crible_residuals[$i];
+    }
+    
+    my($residual)=$i;
+    print "$prime : $i\n" if($i < 100);
+    my($other_res)=$prime - $i;
+    if( $other_res < $n )
+    {
+      init_residuals(\@crible_residuals,$prime,$i);
+      init_residuals(\@crible_residuals,$prime,$other_res);
+    }
   }
 }
 print "$count_seen_primes\n";
@@ -85,70 +97,24 @@ sub pretty_dec
 
 sub init_residuals
 {
-  my($rcrible,$p,$residual,$rprime_infos)=@_;
+  my($rcrible,$p,$residual)=@_;
   
   # print "--- Cribling $residual % $p ---\n";
   
   
-  my($is_prime)=0;
   for(my($nb)=$residual;$nb <= $#$rcrible;$nb+=$p )
   {
-    next if($$rcrible[$nb] == 1);
-    
-    # if( $nb > sqrt((7* $prime_limit + 1)/2) )
-    # {
-      # $$rcrible[$nb] = 1;
-      # next;
-    # }
-    
-    if($$rcrible[$nb] > 0 )
+    if( $$rcrible[$nb] == 0 )
     {
-      $$rcrible[$nb] /= $p;
-    }
-    else
-    {
-      my($fnb)=f($nb);
-      
-      # if( $p > 5 )
-      # {
-      # print ("$fnb = ".pretty_dec($fnb)."\n");<STDIN>;
-      # }
-      
-      if($fnb == $p)
-      {
-        $is_prime = 1;
-        $$rcrible[$nb] = 1;
-        next;
-      }
-      else
-      {
-        $$rcrible[$nb] = $fnb/$p;
-      }
+      $$rcrible[$nb] = f($nb)/$p;
     }
     
     while( $$rcrible[$nb] %$p == 0)
     {
       $$rcrible[$nb] /=$p;
     }
-    
-    if(exists($$rprime_infos{$$rcrible[$nb]}))
-    {
-      my($high_p)=$$rcrible[$nb];
-      my($high_res)=$nb%$high_p;
-      $$rprime_infos{$high_p}=$high_res;
-      
-      # print ("   Found residual of $high_p (from f($nb) = ".f($nb)." -> $high_res)\n");
-      # my($test)=$high_res * $high_res *2 - 1;
-      # print ("   $high_res * $high_res *2 - 1 = $test = ".($test % $high_p)." % $high_p\n");
-      # <STDIN>;
-      
-      $$rcrible[$nb] = 1;
-    }
-    
-    
   }
   # <STDIN>;
-  return ($is_prime);
 }
 
 sub find_residual
