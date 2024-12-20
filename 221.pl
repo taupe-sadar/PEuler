d02b11b3a70f60735db839fe25a5f9cc7a83ffa9
Remi Bertin <remiber@free.fr>
2024-12-20 17:40:38 +0100

    Reverse lopp, confident about complexity

--- 221.pl
+++ 221.pl
@@ -4,26 +4,90 @@ use Data::Dumper;
 use Prime;
 use Divisors;
 
+my($limit)=10**15;
 
-my($p)=2;
+my(@crible)=(0)x100000;
 
-while($p < 1000)
+my($count)=0;
+for( my($d)=1; ; $d++ )
 {
-  my($frac)=$p*$p + 1;
-  my(%dec)=Prime::decompose($frac);
-  print "$p : $frac (".join(' x ',keys(%dec)).")\n";
-
-  my(@divs)=Prime::all_divisors_no_larger(\%dec,$p);
-  # print Dumper \@divs;
-  for(my($i)=0;$i<=$#divs;$i++)
-  {
-    my($r)=$p - $divs[$i];
-    my($q)=$frac/$divs[$i] - $p;
-    my($a)=$p*$q*$r;
-    print "---> $p $q $r => $a\n";
-  }
-
+  my($dcount)=0;
   
+  my($mini_alex)=alexandrian(5*$d/2, $d);
+  last unless($mini_alex < $limit);
+
+  next if($crible[$d] < 0);
+  my($first_p)=fetch_residual($d);
+  if( $first_p == -1 )
+  {
+    my($mult)=$d;
+    while($mult <= $#crible)
+    {
+      $crible[$mult]=-1;
+      $mult+=$d;
+    }
+    next;
+  }
+  
+  next if($first_p == -1);
+  
+  my($alt)=($d==1)?0:($d - 2*$first_p);
+  
+  print "[$d : $first_p] ($alt)\n";
+
+  my($p)=2*$d + $first_p;
+  
+  while(1)
+  {
+    my($alex)=alexandrian($p,$d);
+    last unless($alex < $limit );
+    alex_trace($p,$d);
+    $dcount ++;
+    
+    if($alt > 0)
+    {
+      $alex=alexandrian($p+$alt,$d);
+      last unless($alex < $limit);
+      alex_trace($p+$alt,$d);
+      $dcount++;
+    }
+    $p+=$d;
+  }
+  print "-------------\n";
+  print "$dcount\n";
+  print "-------------\n";
+  $count+= $dcount;
   <STDIN>;
-  $p++;
+  
 }
+print $count;
+
+sub alexandrian
+{
+  my($p,$d)=@_;
+  my($frac)=$p*$p + 1;
+  return $p * ($frac/$d - $p) * ($p - $d);
+}
+
+sub alex_trace
+{
+  my($p,$d)=@_;
+
+  my($frac)=$p*$p + 1;
+  my($r)=$p - $d;
+  my($q)=$frac/$d - $p;
+  my($a)=$p*$q*$r;
+  print "---> $p $q $r => $a\n";
+}
+
+sub fetch_residual
+{
+  my($d)=@_;
+  for(my($x)=0;$x<=$d/2;$x++)
+  {
+    return $x if(($x*$x+1)%$d == 0);
+  }
+  return -1;
+}
+
+
