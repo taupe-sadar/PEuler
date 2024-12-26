4a174c7e189f9e5c2bd1b4d19fed82cc72f2f491
Remi Bertin <remiber@free.fr>
2024-12-26 13:47:26 +0100

    Almost finished, need global loop, and update of counts

--- 221.pl
+++ 221.pl
@@ -6,107 +6,114 @@ use Solver;
 use Prime;
 use Divisors;
 
-my($limit)=10**15;
 
-my(@crible)=(0)x100000;
+my(@crible)=(0)x200000;
 
 my(@divisors)=();
 my(%residuals)=();
 
+count_alex(\@divisors,\%residuals,100000);
 
-my($count)=0;
-for( my($d)=1; $d <= $#crible; $d++ )
+sub count_alex
 {
-  
-  my($mini_alex)=(4*$d + 2)*$d*$d;
-  last unless($mini_alex < $limit);
-  # last unless($d < 1000000);
+  my($rdivisors,$rresiduals,$limit)=@_;
 
-  next if($crible[$d] < 0);
-  my($first_p)=fetch_residual($d);
-  if( $first_p == -1 )
+  my($count)=0;
+  
+  my($div_index)=0;
+  my($d)=0;
+  while( 1 )
   {
-    my($mult)=$d;
-    while($mult <= $#crible)
+    if($div_index <= $$rdivisors) 
     {
-      $crible[$mult]=-1;
-      $mult+=$d;
+      $d = $$rdivisors[$div_index];
     }
-    next;
-  }
-  else
-  {
-    $crible[$d]=$first_p;
-  }
-  
-  
-  
-  my($alt)=($d==1)?0:($d - 2*$first_p);
-  
-  
-
-  push(@divisors,$d);
-  $residuals{$d} = [];
-  $residuals{$d}[0] = $first_p;
-  $residuals{$d}[3] = ($alt > 0)? ($first_p + $alt) : -1;
-
-
-
-  my($init)=2*$d + $first_p;
-
-
-  #new implem : 
-  my($last)=find_count_alex($d,$limit);
-  my($count1)=floor(($last-$init)/$d) + 1;
-  my($count2)=($alt > 0)?(floor(($last-$init - $alt)/$d) + 1):0;
-  
-  print "[$d : $first_p] ($alt) : $count1 + $count2\n";
-  <STDIN>;
-  $count+= $count1 + $count2;
-  if( 0 )
-  {
-  
-
-  
-
-  my($p)=$init;
-  #old implem : test
-  my($dcount1)=0;
-  my($dcount2)=0;
-  while(1)
-  {
-    my($alex)=alexandrian($p,$d);
-    last unless($alex < $limit );
-    # alex_trace($p,$d);
-    $dcount1 ++;
+    else
+    {
+      $d++;
+    }
     
-    if($alt > 0)
-    {
-      $alex=alexandrian($p+$alt,$d);
-      last unless($alex < $limit);
-      # alex_trace($p+$alt,$d);
-      $dcount2++;
-    }
-    $p+=$d;
-  }
-  
-  
-  if($count1!=$dcount1 || ($alt > 0 && $count2!=$dcount2))
-  {
-  print "-------------\n";
-  print "$count1 $count2\n";
-  print "$dcount1 $dcount2\n";
-  print "-------------\n";
-  <STDIN>;
-  }
-  
-  
-  }
-  
-}
-print $count;
+    my($mini_alex)=(4*$d + 2)*$d*$d;
+    last unless($mini_alex < $limit);
+    die "out of crible ($d)!" if ($d > $#crible);
 
-sub find_count_alex
+    next if($crible[$d] < 0);
+    my($first_p)=fetch_residual($d);
+    if( $first_p == -1 )
+    {
+      my($mult)=$d;
+      while($mult <= $#crible)
+      {
+        $crible[$mult]=-1;
+        $mult+=$d;
+      }
+      next;
+    }
+    else
+    {
+      $crible[$d]=$first_p;
+    }
+    
+    
+    
+    
+
+
+    my($init)=2*$d + $first_p;
+
+
+    #new implem : 
+    my($last)=find_limit_alex($d,$limit);
+    my($count1)=floor(($last-$init)/$d) + 1;
+    my($count2)=($alt > 0)?(floor(($last-$init - $alt)/$d) + 1):0;
+    die "WTF " if( $count2 < 0);
+    push(@$rdivisors,$d);
+    $$residuals{$d} = [$count1,$count2];
+
+
+
+    print "[$d : $first_p] ($alt) : $count1 + $count2\n";
+    <STDIN>;
+    $count+= $count1 + $count2;
+    if( 0 )
+    {
+      my($p)=$init;
+      #old implem : test
+      my($dcount1)=0;
+      my($dcount2)=0;
+      while(1)
+      {
+        my($alex)=alexandrian($p,$d);
+        last unless($alex < $limit );
+        # alex_trace($p,$d);
+        $dcount1 ++;
+        
+        if($alt > 0)
+        {
+          $alex=alexandrian($p+$alt,$d);
+          last unless($alex < $limit);
+          # alex_trace($p+$alt,$d);
+          $dcount2++;
+        }
+        $p+=$d;
+      }
+      
+      
+      if($count1!=$dcount1 || ($alt > 0 && $count2!=$dcount2))
+      {
+      print "-------------\n";
+      print "$count1 $count2\n";
+      print "$dcount1 $dcount2\n";
+      print "-------------\n";
+      <STDIN>;
+      }
+    }
+  }
+  print "$limit : $count\n";
+  return $count;
+}
+
+sub find_limit_alex
 {
   my($div,$stop)=@_;
   
